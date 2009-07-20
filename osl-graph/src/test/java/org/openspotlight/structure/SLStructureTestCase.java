b2d5403692f230a113fc0e147d468a6767cba1da
Vitor Hugo Chagas <vitorchagas@gmail.com>
2009-07-20 21:37:51 +0000

    git-svn-id: https://openspotlight.dev.java.net/svn/openspotlight/trunk/source/osl@44 65b4daa9-cd5d-0410-b3a2-f97e3e1d3c1c

--- osl-graph/src/test/java/org/openspotlight/structure/SLStructureTestCase.java
+++ osl-graph/src/test/java/org/openspotlight/structure/SLStructureTestCase.java
@@ -1,21 +1,10 @@
 package org.openspotlight.structure;
 
 import static org.junit.Assert.assertEquals;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
+
 import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+
 import org.junit.Test;
-import org.openspotlight.structure.SLStructure;
-import org.openspotlight.structure.SLStructureImpl;
 import org.openspotlight.structure.elements.SLProperty;
 import org.openspotlight.structure.elements.SLSimpleType;
 import org.openspotlight.structure.test.domain.link.ClassDeclares;
