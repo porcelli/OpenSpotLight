742cfce4a3b277a2111834ae444db14fe0022b47
Luiz Fernando Teston - aka Feu Teston <feu.teston@gmail.com>
2009-09-17 01:48:33 +0000

    Java nodes and links with the correct attributes for java initial import (jar metadata)
    git-svn-id: https://openspotlight.dev.java.net/svn/openspotlight/trunk/source/osl@270 65b4daa9-cd5d-0410-b3a2-f97e3e1d3c1c

---
+++ bundle-processor/osl-java-bundle/src/main/java/org/openspotlight/parser/dap/language/java/link/JavaDocReferences.java
@@ -0,0 +1,71 @@
+
+/*
+ * OpenSpotLight - Open Source IT Governance Platform
+ *  
+ * Copyright (c) 2009, CARAVELATECH CONSULTORIA E TECNOLOGIA EM INFORMATICA LTDA 
+ * or third-party contributors as indicated by the @author tags or express 
+ * copyright attribution statements applied by the authors.  All third-party 
+ * contributions are distributed under license by CARAVELATECH CONSULTORIA E 
+ * TECNOLOGIA EM INFORMATICA LTDA. 
+ * 
+ * This copyrighted material is made available to anyone wishing to use, modify, 
+ * copy, or redistribute it subject to the terms and conditions of the GNU 
+ * Lesser General Public License, as published by the Free Software Foundation. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU Lesser General Public License  for more details. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this distribution; if not, write to: 
+ * Free Software Foundation, Inc. 
+ * 51 Franklin Street, Fifth Floor 
+ * Boston, MA  02110-1301  USA 
+ * 
+ *********************************************************************** 
+ * OpenSpotLight - Plataforma de Governança de TI de Código Aberto 
+ *
+ * Direitos Autorais Reservados (c) 2009, CARAVELATECH CONSULTORIA E TECNOLOGIA 
+ * EM INFORMATICA LTDA ou como contribuidores terceiros indicados pela etiqueta 
+ * @author ou por expressa atribuição de direito autoral declarada e atribuída pelo autor.
+ * Todas as contribuições de terceiros estão distribuídas sob licença da
+ * CARAVELATECH CONSULTORIA E TECNOLOGIA EM INFORMATICA LTDA. 
+ * 
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo sob os 
+ * termos da Licença Pública Geral Menor do GNU conforme publicada pela Free Software 
+ * Foundation. 
+ * 
+ * Este programa é distribuído na expectativa de que seja útil, porém, SEM NENHUMA 
+ * GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU ADEQUAÇÃO A UMA
+ * FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor do GNU para mais detalhes.  
+ * 
+ * Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto com este
+ * programa; se não, escreva para: 
+ * Free Software Foundation, Inc. 
+ * 51 Franklin Street, Fifth Floor 
+ * Boston, MA  02110-1301  USA
+ */
+package org.openspotlight.parser.dap.language.java.link;
+
+import org.openspotlight.graph.annotation.SLProperty;
+import org.openspotlight.graph.SLLink;
+import org.openspotlight.graph.annotation.SLDescription;
+
+/**
+ * The Interface for a link between JavaDoc References.
+ * 
+ *
+ * Java Type A2B Java Type
+ * Java Method A2B 
+ * Java Data A2B 
+ *
+ * @author Luiz Fernando Teston - feu.teston@caravelatech.com 
+ */
+@SLDescription("JavaDoc References")
+public interface JavaDocReferences extends SLLink {
+}
+
+
+        
