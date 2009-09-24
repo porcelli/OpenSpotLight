c732dce182ab5e8aa489381fa6f822ce506e4630
Luiz Fernando Teston - aka Feu Teston <feu.teston@gmail.com>
2009-09-24 13:38:01 +0000

    starting to use mockito...
    git-svn-id: https://openspotlight.dev.java.net/svn/openspotlight/trunk/source/osl@293 65b4daa9-cd5d-0410-b3a2-f97e3e1d3c1c

--- bundle-processor/osl-java-bundle/src/test/java/org/openspotlight/bundle/dap/language/java/JavaTypeFinderTest.java
+++ bundle-processor/osl-java-bundle/src/test/java/org/openspotlight/bundle/dap/language/java/JavaTypeFinderTest.java
@@ -55,85 +55,90 @@ import static java.util.Arrays.asList;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.objectweb.asm.Opcodes;
-import org.openspotlight.bundle.dap.language.java.metamodel.node.JavaType;
+import org.mockito.Mock;
 import org.openspotlight.bundle.dap.language.java.metamodel.node.JavaTypeClass;
 import org.openspotlight.bundle.dap.language.java.metamodel.node.JavaTypeInterface;
 import org.openspotlight.bundle.dap.language.java.metamodel.node.JavaTypePrimitive;
-import org.openspotlight.bundle.dap.language.java.support.JavaGraphNodeSupport;
 import org.openspotlight.bundle.dap.language.java.support.JavaTypeFinder;
 import org.openspotlight.graph.SLContext;
-import org.openspotlight.graph.SLGraph;
-import org.openspotlight.graph.SLGraphFactory;
-import org.openspotlight.graph.SLGraphFactoryImpl;
 import org.openspotlight.graph.SLGraphSession;
+import org.openspotlight.graph.SLNode;
+import org.openspotlight.graph.query.SLQuery;
+import org.openspotlight.graph.query.SLQueryResult;
 
 /**
  * @author Luiz Fernando Teston - feu.teston@caravelatech.com
  */
 public class JavaTypeFinderTest {
 
-    //TODO create a few contexts
+    private JavaTypeFinder javaTypeFinder;
+    @Mock
+    private SLContext      abstractContext;
+    @Mock
+    private SLContext      contextNumberOne;
+    @Mock
+    private SLContext      contextNumberTwo;
+    @Mock
+    private SLContext      contextNumberTree;
+    @Mock
+    private SLGraphSession session;
+    @Mock
+    private SLQuery        query;
+    @Mock
+    private SLQueryResult  result;
 
-    static JavaTypeFinder javaTypeFinder;
-
-    static SLGraph        graph;
-    static SLGraphSession session;
-
-    @BeforeClass
-    public static void setupJavaFinder() throws Exception {
-        final SLGraphFactory factory = new SLGraphFactoryImpl();
-        graph = factory.createTempGraph(false);
-        session = graph.openSession();
-        final SLContext abstractContext = session.createContext(Constants.ABSTRACT_CONTEXT);
-        final SLContext ctx = session.createContext("JRE-util-1.5");
-        final List<SLContext> orderedActiveContexts = asList(ctx);
-
-        javaTypeFinder = new JavaTypeFinder(abstractContext, orderedActiveContexts, true, session);
-        final JavaGraphNodeSupport support = new JavaGraphNodeSupport(session, ctx.getRootNode(), abstractContext.getRootNode());
-        support.addBeforeTypeProcessing(JavaType.class, "java.lang", "Object", Opcodes.ACC_PUBLIC);
-        support.addBeforeTypeProcessing(JavaType.class, "java.lang", "String", Opcodes.ACC_PUBLIC);
-        support.addAfterTypeProcessing(JavaTypeClass.class, "java.lang", "String");
-
-    }
-
-    @AfterClass
-    public static void shutDown() throws Exception {
-        session.close();
-        graph.shutdown();
+    @Before
+    public void setup() throws Exception {
+        initMocks(this);
+        when(this.abstractContext.getID()).thenReturn(Constants.ABSTRACT_CONTEXT);
+        when(this.contextNumberOne.getID()).thenReturn("ctx-1");
+        when(this.contextNumberTwo.getID()).thenReturn("ctx-2");
+        when(this.contextNumberTree.getID()).thenReturn("ctx-3");
+        when(this.session.createQuery()).thenReturn(this.query);
+        when(this.query.execute()).thenReturn(this.result);
+        final List<SLContext> orderedActiveContexts = asList(this.contextNumberOne, this.contextNumberTwo, this.contextNumberTree);
+        this.javaTypeFinder = new JavaTypeFinder(this.abstractContext, orderedActiveContexts, true, this.session);
     }
 
     @Test
     public void shouldFindConcreteClass() throws Exception {
-        final JavaTypeClass stringClass = JavaTypeFinderTest.javaTypeFinder.getType("java.lang.String");
+        final List<SLNode> resultList = new ArrayList<SLNode>();
+        final JavaTypeClass newType = mock(JavaTypeClass.class);
+        when(newType.getContext()).thenReturn(this.contextNumberOne);
+        when(newType.getCompleteName()).thenReturn("java.lang.String");
+        when(this.result.getNodes()).thenReturn(resultList);
+        final JavaTypeClass stringClass = this.javaTypeFinder.getType("java.lang.String");
         assertThat(stringClass, is(notNullValue()));
     }
 
     @Ignore
     @Test
     public void shouldFindConcreteInnerClass() throws Exception {
-        final JavaTypeClass entryClass = JavaTypeFinderTest.javaTypeFinder.getType("java.lang.Map.Entry");
+        final JavaTypeClass entryClass = this.javaTypeFinder.getType("java.lang.Map.Entry");
         assertThat(entryClass, is(notNullValue()));
     }
 
     @Ignore
     @Test
     public void shouldFindInterfaceType() throws Exception {
-        final JavaTypeInterface mapClass = JavaTypeFinderTest.javaTypeFinder.getType("java.util.Map");
+        final JavaTypeInterface mapClass = this.javaTypeFinder.getType("java.util.Map");
         assertThat(mapClass, is(notNullValue()));
     }
 
     @Ignore
     @Test
     public void shouldFindPrimitiveType() throws Exception {
-        final JavaTypePrimitive intClass = JavaTypeFinderTest.javaTypeFinder.getType("int");
+        final JavaTypePrimitive intClass = this.javaTypeFinder.getType("int");
         assertThat(intClass, is(notNullValue()));
     }
 }
