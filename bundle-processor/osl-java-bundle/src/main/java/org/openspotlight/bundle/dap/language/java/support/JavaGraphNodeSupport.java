830af245d3ecdb8e4f183a1ba4ce38c9417b533b
Luiz Fernando Teston - aka Feu Teston <feu.teston@gmail.com>
2009-09-24 13:50:07 +0000

    Javadoc on JavaGraphNodeSupport
    git-svn-id: https://openspotlight.dev.java.net/svn/openspotlight/trunk/source/osl@307 65b4daa9-cd5d-0410-b3a2-f97e3e1d3c1c

--- bundle-processor/osl-java-bundle/src/main/java/org/openspotlight/bundle/dap/language/java/support/JavaGraphNodeSupport.java
+++ bundle-processor/osl-java-bundle/src/main/java/org/openspotlight/bundle/dap/language/java/support/JavaGraphNodeSupport.java
@@ -48,10 +48,14 @@
  */
 package org.openspotlight.bundle.dap.language.java.support;
 
+import static org.openspotlight.common.util.Assertions.checkCondition;
+import static org.openspotlight.common.util.Assertions.checkNotNull;
+
 import java.util.Map;
 import java.util.TreeMap;
 
 import org.objectweb.asm.Opcodes;
+import org.openspotlight.bundle.dap.language.java.Constants;
 import org.openspotlight.bundle.dap.language.java.metamodel.link.AbstractTypeBind;
 import org.openspotlight.bundle.dap.language.java.metamodel.link.DataType;
 import org.openspotlight.bundle.dap.language.java.metamodel.link.Extends;
@@ -69,23 +73,71 @@ import org.openspotlight.bundle.dap.language.java.metamodel.node.JavaPackage;
 import org.openspotlight.bundle.dap.language.java.metamodel.node.JavaType;
 import org.openspotlight.bundle.dap.language.java.metamodel.node.JavaTypePrimitive;
 import org.openspotlight.graph.SLGraphSession;
+import org.openspotlight.graph.SLGraphSessionException;
 import org.openspotlight.graph.SLNode;
 
+/**
+ * This class should be used to insert all java relationships on the OSL Graph instead of inserting nodes and links by hand. This
+ * class creates all necessary links and nodes, includding the implicit ones. When receiving int parameters on access parameters
+ * inside methods, the {@link Opcodes} constants from ASM should be used. First of all, all types on the current classpath should
+ * be added. After
+ * 
+ * @author Luiz Fernando Teston - feu.teston@caravelatech.com
+ */
 public class JavaGraphNodeSupport {
-    private boolean                     usingCache           = true;
-    private final SLNode                currentContextRootNode;
-    private final SLNode                abstractContextRootNode;
-    private final SLGraphSession        session;
-    private final Map<String, JavaType> nodesFromThisContext = new TreeMap<String, JavaType>();
 
+    /** The using cache. */
+    private boolean                     usingCache               = true;
+
+    /** The current context root node. */
+    private final SLNode                currentContextRootNode;
+
+    /** The abstract context root node. */
+    private final SLNode                abstractContextRootNode;
+
+    /** The session. */
+    private final SLGraphSession        session;
+
+    /** The nodes from this context. */
+    private final Map<String, JavaType> nodesFromThisContext     = new TreeMap<String, JavaType>();
+
+    /** The nodes from abstract context. */
+    private final Map<String, JavaType> nodesFromAbstractContext = new TreeMap<String, JavaType>();
+
+    /**
+     * Instantiates a new java graph node support. The abstractContextRootNode will store all implicit relationships and also all
+     * abstract types will be there. The concrete types will be created on currentContextRootNode. The abstractContextRootNode
+     * should be used as a "global classpath". First thing to use this class in a proper way: creates all types declared in
+     * current classpath on the currentContextRootNode. When adding new implicit types (on throws, or parameter, or field
+     * declarations for example) this types will be created on the abstract context in case where this types doesn't exists on
+     * current context.
+     * 
+     * @param session the session
+     * @param currentContextRootNode the current context root node
+     * @param abstractContextRootNode the abstract context root node
+     * @throws SLGraphSessionException
+     */
     public JavaGraphNodeSupport(
                                  final SLGraphSession session, final SLNode currentContextRootNode,
-                                 final SLNode abstractContextRootNode ) {
+                                 final SLNode abstractContextRootNode ) throws SLGraphSessionException {
+        checkNotNull("session", session);
+        checkNotNull("currentContextRootNode", currentContextRootNode);
+        checkNotNull("abstractContextRootNode", abstractContextRootNode);
+        checkCondition("correctAbstractContext", Constants.ABSTRACT_CONTEXT.equals(abstractContextRootNode.getContext().getID()));
         this.session = session;
         this.currentContextRootNode = currentContextRootNode;
         this.abstractContextRootNode = abstractContextRootNode;
     }
 
+    /**
+     * Adds the extends links and also the implicit relationships.
+     * 
+     * @param packageName the package name
+     * @param typeName the type name
+     * @param superPackageName the super package name
+     * @param superTypeName the super type name
+     * @throws Exception the exception
+     */
     public void addExtendsLinks( final String packageName,
                                  final String typeName,
                                  final String superPackageName,
@@ -95,6 +147,15 @@ public class JavaGraphNodeSupport {
         this.session.addLink(Extends.class, newType, newSuperType, false);
     }
 
+    /**
+     * Adds the implements links and also the implicit relationships.
+     * 
+     * @param packageName the package name
+     * @param typeName the type name
+     * @param superPackageName the super package name
+     * @param superTypeName the super type name
+     * @throws Exception the exception
+     */
     public void addImplementsLinks( final String packageName,
                                     final String typeName,
                                     final String superPackageName,
@@ -105,6 +166,14 @@ public class JavaGraphNodeSupport {
 
     }
 
+    /**
+     * Adds the throws on method and also the implicit relationships.
+     * 
+     * @param method the method
+     * @param exceptionPackage the exception package
+     * @param exceptionName the exception name
+     * @throws Exception the exception
+     */
     public void addThrowsOnMethod( final JavaMethod method,
                                    final String exceptionPackage,
                                    final String exceptionName ) throws Exception {
@@ -113,12 +182,27 @@ public class JavaGraphNodeSupport {
 
     }
 
+    /**
+     * Adds the type on abstract context to be used as a "global classphath element". The types declared on current classpath
+     * should be added with method {@link #addTypeOnCurrentContext(Class, String, String, int)}
+     * 
+     * @param nodeType the node type
+     * @param packageName the package name
+     * @param nodeName the node name
+     * @return the t
+     * @throws Exception the exception
+     */
+    @SuppressWarnings( "unchecked" )
     public <T extends JavaType> T addTypeOnAbstractContext( final Class<T> nodeType,
                                                             final String packageName,
                                                             final String nodeName ) throws Exception {
         if (this.usingCache && this.nodesFromThisContext.containsKey(packageName + nodeName)) {
             return (T)this.nodesFromThisContext.get(packageName + nodeName);
         }
+        if (this.usingCache && this.nodesFromAbstractContext.containsKey(packageName + nodeName)) {
+            return (T)this.nodesFromAbstractContext.get(packageName + nodeName);
+        }
+
         if (JavaTypePrimitive.class.equals(nodeType)) {
             final T newType = this.abstractContextRootNode.addNode(nodeType, nodeName);
             newType.setSimpleName(nodeName);
@@ -130,13 +214,29 @@ public class JavaGraphNodeSupport {
         newType.setSimpleName(nodeName);
         newType.setCompleteName(packageName + "." + nodeName);
         this.session.addLink(PackageType.class, newPackage, newType, false);
+        this.nodesFromAbstractContext.put(packageName + nodeName, newType);
         return newType;
     }
 
+    /**
+     * Adds the type on current context. This method should be used on current classpath elements only. The types declared outside
+     * this classpath should be added with method {@link #addTypeOnAbstractContext(Class, String, String)}.
+     * 
+     * @param nodeType the node type
+     * @param packageName the package name
+     * @param nodeName the node name
+     * @param access the access
+     * @return the t
+     * @throws Exception the exception
+     */
+    @SuppressWarnings( "unchecked" )
     public <T extends JavaType> T addTypeOnCurrentContext( final Class<T> nodeType,
                                                            final String packageName,
                                                            final String nodeName,
                                                            final int access ) throws Exception {
+        if (this.usingCache && this.nodesFromThisContext.containsKey(packageName + nodeName)) {
+            return (T)this.nodesFromThisContext.get(packageName + nodeName);
+        }
         if (JavaTypePrimitive.class.equals(nodeType)) {
             final T newType = this.abstractContextRootNode.addNode(nodeType, nodeName);
             newType.setSimpleName(nodeName);
@@ -163,10 +263,24 @@ public class JavaGraphNodeSupport {
         final JavaType newAbstractType = newAbstractPackage.addNode(JavaType.class, nodeName);
         this.session.addLink(PackageType.class, newPackage, newType, false);
         this.session.addLink(AbstractTypeBind.class, newAbstractType, newType, false);
-
+        this.nodesFromThisContext.put(packageName + nodeName, newType);
         return newType;
     }
 
+    /**
+     * Creates the field and also its links.
+     * 
+     * @param newType the new type
+     * @param fieldType the field type
+     * @param fieldPackage the field package
+     * @param fieldTypeName the field type name
+     * @param fieldName the field name
+     * @param access the access
+     * @param array the array
+     * @param arrayDimensions the array dimensions
+     * @return the java data field
+     * @throws Exception the exception
+     */
     public JavaDataField createField( final JavaType newType,
                                       final Class<? extends JavaType> fieldType,
                                       final String fieldPackage,
@@ -181,6 +295,18 @@ public class JavaGraphNodeSupport {
         return field;
     }
 
+    /**
+     * Creates the method and also its links. The parameters, exceptions and so on should be created with the proper methods on
+     * this class.
+     * 
+     * @param newType the new type
+     * @param methodFullName the method full name
+     * @param methodName the method name
+     * @param constructor the constructor
+     * @param access the access
+     * @return the java method
+     * @throws Exception the exception
+     */
     public JavaMethod createMethod( final JavaType newType,
                                     final String methodFullName,
                                     final String methodName,
@@ -198,6 +324,18 @@ public class JavaGraphNodeSupport {
         return method;
     }
 
+    /**
+     * Creates the method parameter and its links.
+     * 
+     * @param method the method
+     * @param parameterType the parameter type
+     * @param parameterOrder the parameter order
+     * @param parameterPackage the parameter package
+     * @param parameterTypeName the parameter type name
+     * @param array the array
+     * @param arrayDimensions the array dimensions
+     * @throws Exception the exception
+     */
     public void createMethodParameter( final JavaMethod method,
                                        final Class<? extends JavaType> parameterType,
                                        final int parameterOrder,
@@ -214,6 +352,17 @@ public class JavaGraphNodeSupport {
         methodParametersTypeLink.setArrayDimension(arrayDimensions);
     }
 
+    /**
+     * Creates the method return type and its links.
+     * 
+     * @param method the method
+     * @param returnType the return type
+     * @param returnPackageName the return package name
+     * @param returnTypeName the return type name
+     * @param array the array
+     * @param arrayDimension the array dimension
+     * @throws Exception the exception
+     */
     public void createMethodReturnType( final JavaMethod method,
                                         final Class<? extends JavaType> returnType,
                                         final String returnPackageName,
@@ -226,6 +375,16 @@ public class JavaGraphNodeSupport {
         methodReturnsType.setArrayDimension(arrayDimension);
     }
 
+    /**
+     * Insert field data.
+     * 
+     * @param field the field
+     * @param fieldType the field type
+     * @param access the access
+     * @param isArray the is array
+     * @param dimension the dimension
+     * @throws Exception the exception
+     */
     private void insertFieldData( final JavaDataField field,
                                   final JavaType fieldType,
                                   final int access,
@@ -250,10 +409,21 @@ public class JavaGraphNodeSupport {
         field.setVolatile(isFieldVolatile);
     }
 
+    /**
+     * Checks if is using cache.
+     * 
+     * @return true, if is using cache
+     */
     public boolean isUsingCache() {
         return this.usingCache;
     }
 
+    /**
+     * Sets the method data.
+     * 
+     * @param method the method
+     * @param access the access
+     */
     private void setMethodData( final JavaMethod method,
                                 final int access ) {
         final boolean isMethodPublic = (access & Opcodes.ACC_PUBLIC) != 0;
@@ -270,6 +440,11 @@ public class JavaGraphNodeSupport {
         method.setSynchronized(isMethodSynchronized);
     }
 
+    /**
+     * Sets the using cache.
+     * 
+     * @param usingCache the new using cache
+     */
     public void setUsingCache( final boolean usingCache ) {
         this.usingCache = usingCache;
     }
