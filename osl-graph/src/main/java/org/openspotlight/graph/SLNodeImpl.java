5fbb710e231e00fbb5217795cd538999d85fc92d
Vitor Hugo Chagas <vitorchagas@gmail.com>
2009-09-25 22:30:54 +0000

    resolving session issue
    git-svn-id: https://openspotlight.dev.java.net/svn/openspotlight/trunk/source/osl@322 65b4daa9-cd5d-0410-b3a2-f97e3e1d3c1c

--- osl-graph/src/main/java/org/openspotlight/graph/SLNodeImpl.java
+++ osl-graph/src/main/java/org/openspotlight/graph/SLNodeImpl.java
@@ -46,6 +46,7 @@
  * 51 Franklin Street, Fifth Floor 
  * Boston, MA  02110-1301  USA
  */
+
 package org.openspotlight.graph;
 
 import static org.openspotlight.graph.SLCommonSupport.getInternalPropertyAsString;
