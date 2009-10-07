f6b6119d944dbfcac4cdbd9f8eedd44e0830c044
Feu Teston <feu.teston@gmail.com>
2009-10-07 13:41:37 -0300

    new configuration node visitor to mark its nodes as dirty

--- osl-web/src/main/java/org/openspotlight/web/command/InitialImportWebCommand.java
+++ osl-web/src/main/java/org/openspotlight/web/command/InitialImportWebCommand.java
@@ -10,13 +10,12 @@ import javax.jcr.Session;
 import org.openspotlight.common.LazyType;
 import org.openspotlight.common.exception.SLException;
 import org.openspotlight.common.util.ClassPathResource;
-import org.openspotlight.federation.data.ConfigurationNode;
 import org.openspotlight.federation.data.NoConfigurationYetException;
-import org.openspotlight.federation.data.InstanceMetadata.ConfigurationNodeVisitor;
 import org.openspotlight.federation.data.impl.Configuration;
 import org.openspotlight.federation.data.load.ConfigurationManager;
 import org.openspotlight.federation.data.load.JcrSessionConfigurationManager;
 import org.openspotlight.federation.data.load.XmlConfigurationManager;
+import org.openspotlight.federation.util.MarkAllAsDirtyVisitor;
 
 public class InitialImportWebCommand implements WebCommand {
 
@@ -52,11 +51,7 @@ public class InitialImportWebCommand implements WebCommand {
         final InputStream is = ClassPathResource.getResourceFromClassPath("osl-configuration.xml");
         final XmlConfigurationManager xmlManager = new XmlConfigurationManager(is);
         configuration = xmlManager.load(LazyType.EAGER);
-        configuration.getInstanceMetadata().accept(new ConfigurationNodeVisitor() {
-            public void visitNode( final ConfigurationNode node ) {
-                node.getInstanceMetadata().getSharedData().fireNodeChange(node, node);
-            }
-        });
+        configuration.getInstanceMetadata().accept(new MarkAllAsDirtyVisitor());
         manager.save(configuration);
         return configuration;
     }
