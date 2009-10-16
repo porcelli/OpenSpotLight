79f0f61d1cbd941ee78bf611f1d49defebf767ae
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-10-16 11:51:37 -0300

    last test fix

--- osl-common/src/test/java/org/openspotlight/common/util/test/ExampleInterface.java
+++ osl-common/src/test/java/org/openspotlight/common/util/test/ExampleInterface.java
@@ -2,6 +2,10 @@ package org.openspotlight.common.util.test;
 
 public interface ExampleInterface {
 
+    public boolean isFinalized();
+
     public boolean returnsTrue() throws Exception;
+
+    public void setFinalized( boolean finalized );
 
 }
