8734548bf792ce634ac52519c0f490d866f16d84
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-10-18 02:48:18 -0200

    desinging some stuff...

--- osl-remote-support/src/main/java/org/openspotlight/remote/internal/UserToken.java
+++ osl-remote-support/src/main/java/org/openspotlight/remote/internal/UserToken.java
@@ -1,24 +1,83 @@
 package org.openspotlight.remote.internal;
 
+import static org.openspotlight.common.util.Arrays.andOf;
+import static org.openspotlight.common.util.Arrays.of;
+import static org.openspotlight.common.util.Assertions.checkNotEmpty;
+import static org.openspotlight.common.util.Equals.eachEquality;
+import static org.openspotlight.common.util.HashCodes.hashOf;
+
 import java.io.Serializable;
 
+/**
+ * The Class UserToken is necessary for doing remote invocations. This class should not be used directly, since it is stored
+ * Internally by the proxy objects.
+ */
 public class UserToken implements Serializable {
-    private String user;
-    private String token;
 
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = 7576539340354513998L;
+
+    /** The user. */
+    private final String      user;
+
+    /** The token. */
+    private final String      token;
+
+    private final int         hashcode;
+
+    /**
+     * Instantiates a new user token.
+     * 
+     * @param user the user
+     * @param token the token
+     */
+    public UserToken(
+                      final String user, final String token ) {
+        checkNotEmpty("user", user);
+        checkNotEmpty("token", token);
+        this.user = user;
+        this.token = token;
+        this.hashcode = hashOf(user, token);
+    }
+
+    @Override
+    public boolean equals( final Object obj ) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof UserToken)) {
+            return false;
+        }
+        final UserToken that = (UserToken)obj;
+        return eachEquality(of(this.user, this.token), andOf(that.user, that.token));
+    }
+
+    /**
+     * Gets the token.
+     * 
+     * @return the token
+     */
     public String getToken() {
         return this.token;
     }
 
+    /**
+     * Gets the user.
+     * 
+     * @return the user
+     */
     public String getUser() {
         return this.user;
     }
 
-    public void setToken( final String token ) {
-        this.token = token;
+    @Override
+    public int hashCode() {
+        return this.hashcode;
     }
 
-    public void setUser( final String user ) {
-        this.user = user;
+    @Override
+    public String toString() {
+        return "UserToken: " + this.user;
     }
+
 }
--- osl-remote-support/src/test/java/org/openspotlight/graph/server/test/ExampleCajoClient.java
+++ osl-remote-support/src/test/java/org/openspotlight/graph/server/test/ExampleCajoClient.java
@@ -4,12 +4,17 @@ import gnu.cajo.invoke.Remote;
 
 public class ExampleCajoClient {
 
-    public static void main( final String[] args ) throws Exception {
+    public static int getResult() throws Exception {
 
         final Object fromServer = Remote.getItem("//127.0.0.1:1198/server");
         final Object result = Remote.invoke(fromServer, "add3", new Object[] {3});
         final Object strangeResult = result.getClass().getMethod("getI").invoke(result);
-        System.out.println(strangeResult);
+        return (Integer)strangeResult;
+    }
+
+    public static void main( final String... args ) throws Exception {
+        final int result = getResult();
+        System.out.println(result);
 
     }
 
