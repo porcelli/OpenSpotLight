e3ebff3c96cfc7d6d4e351d6cce2b69279ff271c
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-10-20 14:06:08 -0200

    reflection support to find the generic types inside collections or maps on method return types

--- osl-common/src/test/java/org/openspotlight/common/util/test/ReflectionTest.java
+++ osl-common/src/test/java/org/openspotlight/common/util/test/ReflectionTest.java
@@ -57,59 +57,128 @@ import static org.openspotlight.common.util.Reflection.searchType;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 import org.openspotlight.common.util.Reflection;
 import org.openspotlight.common.util.Reflection.InheritanceType;
+import org.openspotlight.common.util.Reflection.UnwrappedCollectionTypeFromMethodReturn;
+import org.openspotlight.common.util.Reflection.UnwrappedMapTypeFromMethodReturn;
 
 /**
  * Test for the class {@link Reflection}
  * 
  * @author Luiz Fernando Teston - feu.teston@caravelatech.com
- * 
  */
-@SuppressWarnings("all")
+@SuppressWarnings( "all" )
 public class ReflectionTest {
-    
+
+    private static class ClassUnderTest {
+
+        public Collection<? super String> collectionString() {
+            return null;
+        }
+
+        public List<String> listString() {
+            return null;
+        }
+
+        public Map<? extends String, ? extends Integer> mapExtendsStringInteger() {
+            return null;
+        }
+
+        public Map<String, Integer> mapStringInteger() {
+            return null;
+        }
+
+        public Map<? super String, ? super Integer> mapSuperStringInteger() {
+            return null;
+        }
+
+        public Set<? extends String> setString() {
+            return null;
+        }
+
+    }
+
     @Test
     public void shouldFindInheritanceTypeOnInheritedClasses() throws Exception {
-        final InheritanceType type = searchInheritanceType(String.class,
-                Integer.class, Serializable.class);
+        final InheritanceType type = searchInheritanceType(String.class, Integer.class, Serializable.class);
         assertThat(type, is(InheritanceType.INHERITED_CLASS));
     }
-    
+
     @Test
     public void shouldFindInheritanceTypeOnTheSameClasses() throws Exception {
-        final InheritanceType type = searchInheritanceType(String.class,
-                Integer.class, String.class);
+        final InheritanceType type = searchInheritanceType(String.class, Integer.class, String.class);
         assertThat(type, is(InheritanceType.SAME_CLASS));
     }
-    
+
     @Test
     public void shouldFindOnInheritedClasses() throws Exception {
-        final Class<?> type = searchType(String.class, Integer.class,
-                Serializable.class);
+        final Class<?> type = searchType(String.class, Integer.class, Serializable.class);
         assertThat(type, is(Serializable.class));
     }
-    
+
     @Test
     public void shouldFindOnTheSameClasses() throws Exception {
-        final Class<?> type = searchType(String.class, Integer.class,
-                String.class);
+        final Class<?> type = searchType(String.class, Integer.class, String.class);
         assertThat(String.class.equals(type), is(true));
     }
-    
+
+    @Test
+    public void shouldFindTypeUnderCollection() throws Exception {
+        final UnwrappedCollectionTypeFromMethodReturn<?> result = Reflection.unwrapCollectionFromMethodReturn(ClassUnderTest.class.getMethod("collectionString"));
+        assertThat(result.getCollectionType().equals(Collection.class), is(true));
+        assertThat(result.getItemType().equals(String.class), is(true));
+    }
+
+    @Test
+    public void shouldFindTypeUnderList() throws Exception {
+        final UnwrappedCollectionTypeFromMethodReturn<?> result = Reflection.unwrapCollectionFromMethodReturn(ClassUnderTest.class.getMethod("listString"));
+        assertThat(result.getCollectionType().equals(List.class), is(true));
+        assertThat(result.getItemType().equals(String.class), is(true));
+    }
+
+    @Test
+    public void shouldFindTypeUnderMap() throws Exception {
+        final UnwrappedMapTypeFromMethodReturn<?, ?> result = Reflection.unwrapMapFromMethodReturn(ClassUnderTest.class.getMethod("mapStringInteger"));
+        assertThat(result.getItemType().getK1().equals(String.class), is(true));
+        assertThat(result.getItemType().getK2().equals(Integer.class), is(true));
+    }
+
+    @Test
+    public void shouldFindTypeUnderMapExtends() throws Exception {
+        final UnwrappedMapTypeFromMethodReturn<?, ?> result = Reflection.unwrapMapFromMethodReturn(ClassUnderTest.class.getMethod("mapExtendsStringInteger"));
+        assertThat(result.getItemType().getK1().equals(String.class), is(true));
+        assertThat(result.getItemType().getK2().equals(Integer.class), is(true));
+    }
+
+    @Test
+    public void shouldFindTypeUnderMapSuper() throws Exception {
+        final UnwrappedMapTypeFromMethodReturn<?, ?> result = Reflection.unwrapMapFromMethodReturn(ClassUnderTest.class.getMethod("mapSuperStringInteger"));
+        assertThat(result.getItemType().getK1().equals(String.class), is(true));
+        assertThat(result.getItemType().getK2().equals(Integer.class), is(true));
+    }
+
+    @Test
+    public void shouldFindTypeUnderSet() throws Exception {
+        final UnwrappedCollectionTypeFromMethodReturn<?> result = Reflection.unwrapCollectionFromMethodReturn(ClassUnderTest.class.getMethod("setString"));
+        assertThat(result.getCollectionType().equals(Set.class), is(true));
+        assertThat(result.getItemType().equals(String.class), is(true));
+    }
+
     @Test
     public void shouldNotFindInheritanceTypeOnStrangeClasses() throws Exception {
-        final InheritanceType type = searchInheritanceType(String.class,
-                Integer.class, Double.class, BigDecimal.class);
+        final InheritanceType type = searchInheritanceType(String.class, Integer.class, Double.class, BigDecimal.class);
         assertThat(type, is(InheritanceType.NO_INHERITANCE));
     }
-    
+
     @Test
     public void shouldNotFindOnStrangeClasses() throws Exception {
-        final Class<?> type = searchType(String.class, Integer.class,
-                Double.class, BigDecimal.class);
+        final Class<?> type = searchType(String.class, Integer.class, Double.class, BigDecimal.class);
         assertThat(type, is(nullValue()));
     }
 }
