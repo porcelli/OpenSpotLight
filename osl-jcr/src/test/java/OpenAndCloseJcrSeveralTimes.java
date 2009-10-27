94b6bd51a8a93ef9a9e72b6c7b7df513fd2bcb3e
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-10-27 11:02:41 -0200

    putting the config files in the right place...

---
+++ osl-jcr/src/test/java/OpenAndCloseJcrSeveralTimes.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import org.openspotlight.jcr.provider.DefaultJcrDescriptor;
+import org.openspotlight.jcr.provider.JcrConnectionDescriptor;
+import org.openspotlight.jcr.provider.JcrConnectionProvider;
+
+public class OpenAndCloseJcrSeveralTimes {
+
+    @Test
+    public void shouldOpenAndCloseJcrSeveralTimes() throws Exception {
+        final JcrConnectionDescriptor desc = DefaultJcrDescriptor.TEMP_DESCRIPTOR;
+        JcrConnectionProvider provider = JcrConnectionProvider.createFromData(desc);
+        provider.openRepository();
+        provider.closeRepository();
+        provider = JcrConnectionProvider.createFromData(desc);
+        provider.openRepository();
+        provider.closeRepository();
+        provider = JcrConnectionProvider.createFromData(desc);
+        provider.openRepository();
+        provider.closeRepository();
+
+    }
+
+}
