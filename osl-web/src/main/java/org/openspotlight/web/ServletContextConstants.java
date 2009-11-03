04ad8084ef1b568a05e5cf50139afeb87ec2f5f8
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-11-03 10:55:34 -0200

    no compiler errors due security refactoring. Let's see if the tests still running ;-)

--- osl-web/src/main/java/org/openspotlight/web/ServletContextConstants.java
+++ osl-web/src/main/java/org/openspotlight/web/ServletContextConstants.java
@@ -1,16 +1,23 @@
 package org.openspotlight.web;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Interface ServletContextConstants.
  */
 public interface ServletContextConstants {
 
     /** The PROVIDER. */
-    String PROVIDER  = "provider";
+    String PROVIDER           = "provider";
 
     /** The GRAPH. */
-    String GRAPH     = "graph";
+    String GRAPH              = "graph";
 
     /** The SCHEDULER. */
-    String SCHEDULER = "scheduler";
+    String SCHEDULER          = "scheduler";
+
+    /** The AUTHENTICATE d_ user. */
+    String AUTHENTICATED_USER = "authenticated_user";
+
+    /** The JC r_ descriptor. */
+    String JCR_DESCRIPTOR     = "jcr_descriptor";
 }
