2106668c25f1e649a5383345b486294ea953afc2
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-11-04 12:31:57 -0200

    SimplePersistSupport working with simple types. Missing collection and node types.

---
+++ osl-simple-persist/src/test/java/org/openspotlight/persist/test/LevelThreeObj.java
@@ -0,0 +1,38 @@
+package org.openspotlight.persist.test;
+
+import org.openspotlight.persist.annotation.KeyProperty;
+import org.openspotlight.persist.annotation.ParentProperty;
+
+public class LevelThreeObj {
+    private String      key;
+
+    private String      property;
+
+    private LevelTwoObj parentObj;
+
+    @KeyProperty
+    public String getKey() {
+        return this.key;
+    }
+
+    @ParentProperty
+    public LevelTwoObj getLevelTwoObj() {
+        return this.parentObj;
+    }
+
+    public String getProperty() {
+        return this.property;
+    }
+
+    public void setKey( final String key ) {
+        this.key = key;
+    }
+
+    public void setLevelTwoObj( final LevelTwoObj parentObj ) {
+        this.parentObj = parentObj;
+    }
+
+    public void setProperty( final String property ) {
+        this.property = property;
+    }
+}
---
+++ osl-simple-persist/src/test/java/org/openspotlight/persist/test/RootObj.java
@@ -0,0 +1,5 @@
+package org.openspotlight.persist.test;
+
+public class RootObj {
+
+}
