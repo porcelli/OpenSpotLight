481c293d0b2355bf026ee8bec69e479c8e3c2417
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-11-11 17:33:09 -0200

    fixing some compiler errors on db tests

--- osl-federation/loader/osl-federation-database-loader/src/test/java/org/openspotlight/federation/data/load/db/test/OracleDatabaseStreamTest.java
+++ osl-federation/loader/osl-federation-database-loader/src/test/java/org/openspotlight/federation/data/load/db/test/OracleDatabaseStreamTest.java
@@ -7,60 +7,55 @@ import java.util.Set;
 
 import org.openspotlight.federation.data.load.db.ScriptType;
 import org.openspotlight.federation.domain.DbArtifactSource;
+import org.openspotlight.federation.domain.Repository;
 
-@SuppressWarnings("all")
-public class OracleDatabaseStreamTest extends DatabaseStreamTest implements
-		RunWhenDatabaseVendorTestsIsActive {
+@SuppressWarnings( "all" )
+public class OracleDatabaseStreamTest extends DatabaseStreamTest implements RunWhenDatabaseVendorTestsIsActive {
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	protected DbArtifactSource createValidConfigurationWithMappings() {
-		final Configuration configuration = createOracleOciDbConfiguration();
-		return (DbArtifactSource) configuration
-				.getRepositoryByName("oracle Repository") //$NON-NLS-1$
-				.getGroupByName("oracle Group") //$NON-NLS-1$
-				.getArtifactSourceByName("oracle Connection"); //$NON-NLS-1$
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected DbArtifactSource createValidConfigurationWithMappings() {
+        final Repository repository = createOracleOciDbConfiguration(); //$NON-NLS-1$
+        return (DbArtifactSource)repository.getGroups().get("oracle Group").getArtifactSources().iterator().next(); //$NON-NLS-1$
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	protected Set<ScriptType> typesToAssert() {
-		return EnumSet.of(ScriptType.TABLE, ScriptType.TRIGGER,
-				ScriptType.PROCEDURE, ScriptType.FUNCTION, ScriptType.VIEW,
-				ScriptType.INDEX, ScriptType.SEQUENCE, ScriptType.CONSTRAINT,
-				ScriptType.PACKAGE, ScriptType.FK);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Set<ScriptType> typesToAssert() {
+        return EnumSet.of(ScriptType.TABLE, ScriptType.TRIGGER, ScriptType.PROCEDURE, ScriptType.FUNCTION, ScriptType.VIEW,
+                          ScriptType.INDEX, ScriptType.SEQUENCE, ScriptType.CONSTRAINT, ScriptType.PACKAGE, ScriptType.FK);
+    }
 
-	// @Override
-	// protected void fillDatabase(Connection conn) throws Exception {
-	// conn.prepareStatement(
-	// "CREATE TABLE example_table ( id INT,  data VARCHAR(100) ) ")
-	// .execute();
-	// conn.prepareStatement(
-	// "CREATE TRIGGER example_trigger BEFORE INSERT ON example_table "
-	// + " FOR EACH ROW BEGIN "
-	// + "   update example_table set id=1; " + " END")
-	// .execute();
-	// conn.prepareStatement(
-	// "CREATE PROCEDURE example_proc (OUT param1 INT) " + " BEGIN "
-	// + "  SELECT COUNT(*) INTO param1 FROM example_table; "
-	// + "END").execute();
-	// conn.prepareStatement(
-	// " CREATE FUNCTION example_function (n1 INT, n2 INT, n3 INT, n4 INT) "
-	// + "   RETURNS INT " + "    DETERMINISTIC "
-	// + "     BEGIN " + "      return n1; " + "     END").execute();
-	// }
-	//
-	// @Override
-	// protected void resetDatabase(Connection conn) throws Exception {
-	// conn.prepareStatement("drop TRIGGER example_trigger ").execute();
-	// conn.prepareStatement("drop TABLE example_table ").execute();
-	// conn.prepareStatement("drop PROCEDURE example_proc ").execute();
-	// conn.prepareStatement(" drop FUNCTION example_function ").execute();
-	// }
+    // @Override
+    // protected void fillDatabase(Connection conn) throws Exception {
+    // conn.prepareStatement(
+    // "CREATE TABLE example_table ( id INT,  data VARCHAR(100) ) ")
+    // .execute();
+    // conn.prepareStatement(
+    // "CREATE TRIGGER example_trigger BEFORE INSERT ON example_table "
+    // + " FOR EACH ROW BEGIN "
+    // + "   update example_table set id=1; " + " END")
+    // .execute();
+    // conn.prepareStatement(
+    // "CREATE PROCEDURE example_proc (OUT param1 INT) " + " BEGIN "
+    // + "  SELECT COUNT(*) INTO param1 FROM example_table; "
+    // + "END").execute();
+    // conn.prepareStatement(
+    // " CREATE FUNCTION example_function (n1 INT, n2 INT, n3 INT, n4 INT) "
+    // + "   RETURNS INT " + "    DETERMINISTIC "
+    // + "     BEGIN " + "      return n1; " + "     END").execute();
+    // }
+    //
+    // @Override
+    // protected void resetDatabase(Connection conn) throws Exception {
+    // conn.prepareStatement("drop TRIGGER example_trigger ").execute();
+    // conn.prepareStatement("drop TABLE example_table ").execute();
+    // conn.prepareStatement("drop PROCEDURE example_proc ").execute();
+    // conn.prepareStatement(" drop FUNCTION example_function ").execute();
+    // }
 
 }
