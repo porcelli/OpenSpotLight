0478abb05581b0bbbbbc18b04b2e3bb075935f15
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-11-16 11:34:45 -0200

    ooops! Pair and Triple classes didn't have equals and hashcode... My fault!

--- osl-simple-persist/src/test/java/org/openspotlight/persist/test/SimplePersistSupportTest.java
+++ osl-simple-persist/src/test/java/org/openspotlight/persist/test/SimplePersistSupportTest.java
@@ -120,6 +120,7 @@ public class SimplePersistSupportTest {
         obj2.setValue(33);
         levelThree.getObjList().add(obj2);
         asJcr = SimplePersistSupport.convertBeanToJcr("a/b/c", this.session, levelThree);
+
         anotherLevelThree = SimplePersistSupport.convertJcrToBean(this.session, asJcr, LazyType.LAZY);
         assertThat(anotherLevelThree.getObjList().size(), is(2));
         assertThat(anotherLevelThree.getObjList().get(0).getValue(), is(obj1.getValue()));
