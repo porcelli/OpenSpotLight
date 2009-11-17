c034a719922027c071a0e3ddadaf0f3983045df1
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-11-17 17:21:35 -0200

    new bundle processing arch... api :-)

--- osl-federation/osl-federation-core/src/test/java/org/openspotlight/federation/loader/ExampleBundleProcessor.java
+++ osl-federation/osl-federation-core/src/test/java/org/openspotlight/federation/loader/ExampleBundleProcessor.java
@@ -1,30 +1,43 @@
 package org.openspotlight.federation.loader;
 
-import org.openspotlight.federation.data.processing.BundleProcessingFatalException;
-import org.openspotlight.federation.data.processing.BundleProcessingNonFatalException;
 import org.openspotlight.federation.data.processing.BundleProcessor;
 import org.openspotlight.federation.domain.Artifact;
+import org.openspotlight.federation.domain.LastProcessStatus;
 import org.openspotlight.federation.domain.StreamArtifact;
 
 public class ExampleBundleProcessor implements BundleProcessor<StreamArtifact> {
 
-    public void globalProcessingFinalized( final org.openspotlight.federation.data.processing.BundleProcessor.BundleProcessingGroup<? extends Artifact> bundleProcessingGroup,
-                                           final org.openspotlight.federation.data.processing.BundleProcessor.BundleProcessingContext graphContext ) {
+    public <A extends Artifact> boolean acceptKindOfArtifact( final Class<A> kindOfArtifact ) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public void afterProcessArtifact( final StreamArtifact artifact,
+                                      final LastProcessStatus status ) {
         // TODO Auto-generated method stub
 
     }
 
-    public org.openspotlight.federation.data.processing.BundleProcessor.ProcessingStartAction globalProcessingStarted( final org.openspotlight.federation.data.processing.BundleProcessor.BundleProcessingGroup<StreamArtifact> bundleProcessingGroup,
-                                                                                                                       final org.openspotlight.federation.data.processing.BundleProcessor.BundleProcessingContext graphContext )
-        throws BundleProcessingFatalException {
+    public void beforeProcessArtifact( final StreamArtifact artifact ) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void globalProcessingDone( final org.openspotlight.federation.data.processing.BundleProcessor.ArtifactProcessingResults<StreamArtifact> results ) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public LastProcessStatus processArtifact( final StreamArtifact artifact,
+                                              final org.openspotlight.federation.data.processing.BundleProcessor.BundleProcessorContext<StreamArtifact> context )
+        throws Exception {
         // TODO Auto-generated method stub
         return null;
     }
 
-    public org.openspotlight.federation.data.processing.BundleProcessor.ProcessingAction processArtifact( final StreamArtifact targetArtifact,
-                                                                                                          final org.openspotlight.federation.data.processing.BundleProcessor.BundleProcessingGroup<StreamArtifact> bundleProcessingGroup,
-                                                                                                          final org.openspotlight.federation.data.processing.BundleProcessor.BundleProcessingContext graphContext )
-        throws BundleProcessingNonFatalException, BundleProcessingFatalException {
+    public org.openspotlight.federation.data.processing.BundleProcessor.ArtifactsToBeProcessed<StreamArtifact> returnArtifactsToBeProcessed( final org.openspotlight.federation.data.processing.BundleProcessor.ArtifactChanges<StreamArtifact> changes,
+                                                                                                                                             final org.openspotlight.federation.data.processing.BundleProcessor.BundleProcessorContext<StreamArtifact> context,
+                                                                                                                                             final org.openspotlight.federation.data.processing.BundleProcessor.ArtifactsToBeProcessed<StreamArtifact> toBeReturned ) {
         // TODO Auto-generated method stub
         return null;
     }
