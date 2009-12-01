ca46ca886b5412113daae105d7c7843c66332663
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-12-01 14:58:40 -0200

    few tests for graph versioning (need to finish)

---
+++ osl-jcr/src/test/java/org/openspotlight/jcr/provider/test/JcrVersioningTest.java
@@ -0,0 +1,68 @@
+package org.openspotlight.jcr.provider.test;
+
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.Session;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openspotlight.common.util.JCRUtil;
+import org.openspotlight.jcr.provider.DefaultJcrDescriptor;
+import org.openspotlight.jcr.provider.JcrConnectionProvider;
+
+public class JcrVersioningTest {
+
+	private static final String VERSIONABLE_NODE = "newVersionableNode";
+	private static JcrConnectionProvider provider;
+
+	@BeforeClass
+	public static void setup() throws Exception {
+		JcrVersioningTest.provider = JcrConnectionProvider
+				.createFromData(DefaultJcrDescriptor.TEMP_DESCRIPTOR);
+		final Session session = JcrVersioningTest.provider.openSession();
+		Node newVersionableNode;
+		try {
+			newVersionableNode = session.getRootNode().getNode(
+					JcrVersioningTest.VERSIONABLE_NODE);
+		} catch (final PathNotFoundException e) {
+			newVersionableNode = session.getRootNode().addNode(
+					JcrVersioningTest.VERSIONABLE_NODE);
+			JCRUtil.makeReferenceable(newVersionableNode);
+			JCRUtil.makeVersionable(newVersionableNode);
+			session.save();
+			newVersionableNode.checkin();
+		}
+	}
+
+	@Test
+	public void shouldCommitOnlyOneVersion() throws Exception {
+		Assert.fail();
+
+	}
+
+	@Test
+	public void shouldCommitTwoVersions() throws Exception {
+		// Session session = provider.openSession();
+		Assert.fail();
+
+	}
+
+	@Test
+	public void shouldCreateTwoVersionsIndepententOnEachOther()
+			throws Exception {
+		Assert.fail();
+
+	}
+
+	@Test
+	public void shouldDismissTwoVersions() throws Exception {
+		Assert.fail();
+	}
+
+	@Test
+	public void shouldMergeTwoCorrelatedChanges() throws Exception {
+		Assert.fail();
+	}
+
+}
