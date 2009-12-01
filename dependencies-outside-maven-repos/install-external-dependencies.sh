af16e94443d05b74c72995f5bdc278e7b67e6e9a
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-12-01 01:49:33 -0200

    external dependencies. Still need to test by deleting ~/.m2/repository and running a clean build. Gonna do this by tomorrow ;-)

---
+++ dependencies-outside-maven-repos/install-external-dependencies.sh
@@ -0,0 +1,13 @@
+mvn install:install-file -DgroupId=cajo -DartifactId=cajo -Dversion=1.127 -Dpackaging=jar -Dfile=cajo.jar
+mvn install:install-file -DgroupId=org.clapper -DartifactId=ocutil -Dversion=2.4.4b -Dpackaging=jar -Dfile=ocutil-2.4.4b.jar 
+mvn install:install-file -DgroupId=dynamo -DartifactId=dynamo-file-gen -Dversion=1.0.1 -Dpackaging=jar -Dfile=dynamo-file-gen-1.0.1.jar
+mvn install:install-file -DgroupId=org.jboss.identity.idm -DartifactId=idm-api -Dversion=1.0.0-Beta4-SNAPSHOT -Dpackaging=jar -Dfile=idm-api-1.0.0-Beta4-SNAPSHOT.jar
+mvn install:install-file -DgroupId=org.jboss.identity.idm -DartifactId=idm-testsuite -Dversion=1.0.0-Beta4-SNAPSHOT -Dpackaging=jar -Dfile=idm-testsuite-1.0.0-Beta4-SNAPSHOT.jar
+mvn install:install-file -DgroupId=org.jboss.identity.idm -DartifactId=idm-spi -Dversion=1.0.0-Beta4-SNAPSHOT -Dpackaging=jar -Dfile=idm-spi-1.0.0-Beta4-SNAPSHOT.jar
+mvn install:install-file -DgroupId=org.jboss.identity.idm -DartifactId=idm-ldap -Dversion=1.0.0-Beta4-SNAPSHOT -Dpackaging=jar -Dfile=idm-ldap-1.0.0-Beta4-SNAPSHOT.jar
+mvn install:install-file -DgroupId=org.jboss.identity.idm -DartifactId=idm-hibernate -Dversion=1.0.0-Beta4-SNAPSHOT -Dpackaging=jar -Dfile=idm-hibernate-1.0.0-Beta4-SNAPSHOT.jar
+mvn install:install-file -DgroupId=org.jboss.identity.idm -DartifactId=idm-core -Dversion=1.0.0-Beta4-SNAPSHOT -Dpackaging=jar -Dfile=idm-core-1.0.0-Beta4-SNAPSHOT.jar
+mvn install:install-file -DgroupId=org.jboss.dna -DartifactId=dna-common -Dversion=0.6 -Dpackaging=jar -Dfile=dna-common-0.6.jar
+mvn install:install-file -DgroupId=org.jboss.dna -DartifactId=dna-graph -Dversion=0.6 -Dpackaging=jar -Dfile=dna-graph-0.6.jar
+mvn install:install-file -DgroupId=org.jboss.dna -DartifactId=dna-connector-filesystem -Dversion=0.6 -Dpackaging=jar -Dfile=dna-connector-filesystem-0.6.jar
+mvn install:install-file -DgroupId=org.jboss.dna -DartifactId=dna-connector-svn -Dversion=0.6 -Dpackaging=jar -Dfile=dna-connector-svn-0.6.jar
--- dependencies-outside-maven-repos/mvn-install-cajo-command.sh
+++
@@ -1,1 +0,0 @@
-mvn install:install-file -DgroupId=cajo -DartifactId=cajo -Dversion=1.127 -Dpackaging=jar -Dfile=cajo.jar
