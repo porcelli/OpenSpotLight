8651ef176b2ce9bfe16c6f6b102953b010ed8413
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-12-06 11:19:45 -0200

    Just created the new DefaultExecutionContextFactory. Now I need to test it and also use it on Web and BundleManager classes

---
+++ osl-common/src/main/java/org/openspotlight/common/concurrent/DefaultAtomicLazyResource.java
@@ -0,0 +1,24 @@
+package org.openspotlight.common.concurrent;
+
+import org.openspotlight.common.exception.SLRuntimeException;
+
+/**
+ * This class extends {@link AtomicLazyResource} wrapping all exceptions on
+ * {@link SLRuntimeException}
+ * 
+ * @author feu
+ * 
+ * @param <R>
+ */
+public abstract class DefaultAtomicLazyResource<R> extends
+		AtomicLazyResource<R, SLRuntimeException> {
+
+	public DefaultAtomicLazyResource() {
+		super(SLRuntimeException.class);
+	}
+
+	public DefaultAtomicLazyResource(final LockContainer lockContainer) {
+		super(lockContainer, SLRuntimeException.class);
+	}
+
+}
--- osl-federation/osl-federation-core/src/main/java/org/openspotlight/federation/context/DefaultExecutionContext.java
+++ osl-federation/osl-federation-core/src/main/java/org/openspotlight/federation/context/DefaultExecutionContext.java
@@ -1,42 +1,180 @@
 package org.openspotlight.federation.context;
 
-import org.apache.log4j.lf5.viewer.configure.ConfigurationManager;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.openspotlight.common.concurrent.DefaultAtomicLazyResource;
+import org.openspotlight.common.concurrent.LockContainer;
+import org.openspotlight.common.util.AbstractFactory;
 import org.openspotlight.federation.domain.Artifact;
 import org.openspotlight.federation.finder.ArtifactFinder;
+import org.openspotlight.federation.loader.ConfigurationManager;
+import org.openspotlight.federation.loader.JcrSessionConfigurationManagerFactory;
+import org.openspotlight.federation.log.JcrDetailedLogger;
+import org.openspotlight.graph.SLGraph;
+import org.openspotlight.graph.SLGraphFactory;
 import org.openspotlight.graph.SLGraphSession;
+import org.openspotlight.jcr.provider.DefaultJcrDescriptor;
+import org.openspotlight.jcr.provider.JcrConnectionDescriptor;
 import org.openspotlight.jcr.provider.JcrConnectionProvider;
 import org.openspotlight.log.DetailedLogger;
+import org.openspotlight.security.SecurityFactory;
+import org.openspotlight.security.idm.AuthenticatedUser;
+import org.openspotlight.security.idm.User;
 
-public class DefaultExecutionContext implements ExecutionContext {
+/**
+ * This class is an {@link ExecutionContext} which initialize all resources in a
+ * lazy way, and also close it in a lazy way also.
+ * 
+ * @author feu
+ * 
+ */
+public class DefaultExecutionContext implements ExecutionContext, LockContainer {
 
-	public void closeResources() {
-		// TODO Auto-generated method stub
+	public static interface ClosingListener {
+		public void didCloseResources(DefaultExecutionContext context);
+	}
+
+	private final class LazyConfigurationManagerProvider extends
+			DefaultAtomicLazyResource<ConfigurationManager> {
+		private LazyConfigurationManagerProvider(
+				final LockContainer lockContainer) {
+			super(lockContainer);
+		}
+
+		@Override
+		protected ConfigurationManager createReference() {
+			return JcrSessionConfigurationManagerFactory
+					.createMutableUsingSession(JcrConnectionProvider
+							.createFromData(descriptor).openSession());
+		}
+	}
+
+	private final class LazyDetailedLoggerProvider extends
+			DefaultAtomicLazyResource<DetailedLogger> {
+		private LazyDetailedLoggerProvider(final LockContainer lockContainer) {
+			super(lockContainer);
+		}
+
+		@Override
+		protected DetailedLogger createReference() {
+			return new JcrDetailedLogger(JcrConnectionProvider.createFromData(
+					descriptor).openSession());
+		}
+	}
+
+	private final class LazyGraphSessionProvider extends
+			DefaultAtomicLazyResource<SLGraphSession> {
+		private LazyGraphSessionProvider(final LockContainer lockContainer) {
+			super(lockContainer);
+		}
+
+		@Override
+		protected SLGraphSession createReference() throws Exception {
+			final SecurityFactory securityFactory = AbstractFactory
+					.getDefaultInstance(SecurityFactory.class);
+			final User simpleUser = securityFactory.createUser(username);
+			final AuthenticatedUser user = securityFactory
+					.createIdentityManager(DefaultJcrDescriptor.TEMP_DESCRIPTOR)
+					.authenticate(simpleUser, password);
+			final SLGraph graph = AbstractFactory.getDefaultInstance(
+					SLGraphFactory.class).createGraph(descriptor);
+			return graph.openSession(user, repositoryName);
+		}
+	}
+
+	private final class LazyJcrConnectionProvider extends
+			DefaultAtomicLazyResource<JcrConnectionProvider> {
+		private LazyJcrConnectionProvider(final LockContainer lockContainer) {
+			super(lockContainer);
+		}
+
+		@Override
+		protected JcrConnectionProvider createReference() {
+			return JcrConnectionProvider.createFromData(descriptor);
+		}
+	}
+
+	private final String username;
+	private final String password;
+	private final JcrConnectionDescriptor descriptor;
+	private final String repositoryName;
+	private final ClosingListener listener;
+	private final Object lock = new Object();
+	private final ConcurrentHashMap<? extends Artifact, DefaultAtomicLazyResource<ArtifactFinder<? extends Artifact>>> artifactFinderReferences = new ConcurrentHashMap<Artifact, DefaultAtomicLazyResource<ArtifactFinder<? extends Artifact>>>();
+
+	private final DefaultAtomicLazyResource<JcrConnectionProvider> lazyConnectionProviderReference = new LazyJcrConnectionProvider(
+			this);
+
+	private final DefaultAtomicLazyResource<ConfigurationManager> lazyConfigurationManagerReference = new LazyConfigurationManagerProvider(
+			this);
+
+	private final DefaultAtomicLazyResource<SLGraphSession> lazyGraphSessionReference = new LazyGraphSessionProvider(
+			this);
+
+	private final DefaultAtomicLazyResource<DetailedLogger> lazyDetailedLoggerReference = new LazyDetailedLoggerProvider(
+			this);
+
+	public DefaultExecutionContext(final String username,
+			final String password, final JcrConnectionDescriptor descriptor,
+			final String repositoryName, final ClosingListener listener) {
+		this.username = username;
+		this.password = password;
+		this.descriptor = descriptor;
+		this.repositoryName = repositoryName;
+		this.listener = listener;
 
 	}
 
-	public <A extends Artifact> ArtifactFinder<A> getDefaultArtifactFinder() {
-		// TODO Auto-generated method stub
-		return null;
+	public void closeResources() {
+		synchronized (lock) {
+			for (final DefaultAtomicLazyResource<ArtifactFinder<? extends Artifact>> lazyReference : artifactFinderReferences
+					.values()) {
+				lazyReference.closeResources();
+			}
+			lazyConfigurationManagerReference.closeResources();
+			lazyDetailedLoggerReference.closeResources();
+			lazyGraphSessionReference.closeResources();
+			lazyConnectionProviderReference.closeResources();
+			listener.didCloseResources(this);
+		}
+	}
+
+	public <A extends Artifact> ArtifactFinder<A> getDefaultArtifactFinder(
+			final Class<A> type) {
+		synchronized (lock) {
+			DefaultAtomicLazyResource<ArtifactFinder<? extends Artifact>> lazyReference = artifactFinderReferences
+					.get(type);
+			if (lazyReference == null) {
+				lazyReference = new DefaultAtomicLazyResource<ArtifactFinder<? extends Artifact>>() {
+
+					@Override
+					protected ArtifactFinder<? extends Artifact> createReference() {
+						return null;
+					}
+				};
+			}
+			return (ArtifactFinder<A>) lazyReference.get();
+		}
 	}
 
 	public ConfigurationManager getDefaultConfigurationManager() {
-		// TODO Auto-generated method stub
-		return null;
+		return lazyConfigurationManagerReference.get();
 	}
 
 	public JcrConnectionProvider getDefaultConnectionProvider() {
-		// TODO Auto-generated method stub
-		return null;
+		return lazyConnectionProviderReference.get();
 	}
 
 	public SLGraphSession getGraphSession() {
-		// TODO Auto-generated method stub
-		return null;
+		return lazyGraphSessionReference.get();
+	}
+
+	public Object getLockObject() {
+		return lock;
 	}
 
 	public DetailedLogger getLogger() {
-		// TODO Auto-generated method stub
-		return null;
+		return lazyDetailedLoggerReference.get();
 	}
 
 }
