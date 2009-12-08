0c2e49116b5e273269e4748bcabe6d1ddda225df
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-12-08 18:03:28 -0200

    fixing three tests I just broke :-)

--- osl-federation/osl-federation-core/src/test/java/org/openspotlight/federation/loader/ArtifactLoaderTest.java
+++ osl-federation/osl-federation-core/src/test/java/org/openspotlight/federation/loader/ArtifactLoaderTest.java
@@ -61,45 +61,54 @@ import org.openspotlight.federation.domain.Artifact;
 import org.openspotlight.federation.domain.ArtifactSource;
 import org.openspotlight.federation.domain.ArtifactSourceMapping;
 import org.openspotlight.federation.domain.GlobalSettings;
+import org.openspotlight.federation.domain.Repository;
 import org.openspotlight.federation.finder.FileSystemArtifactBySourceProvider;
 
 public class ArtifactLoaderTest {
 
-    @Test
-    public void shouldLoad() throws Exception {
-        final GlobalSettings configuration = new GlobalSettings();
-        configuration.setDefaultSleepingIntervalInMilliseconds(500);
-        configuration.setNumberOfParallelThreads(4);
-        final String initialRawPath = Files.getNormalizedFileName(new File("../.."));
-        final String initial = initialRawPath.substring(0, initialRawPath.lastIndexOf('/'));
-        final String finalStr = initialRawPath.substring(initial.length());
-        final ArtifactSource source = new ArtifactSource();
-        final ArtifactSourceMapping mapping = new ArtifactSourceMapping();
-        mapping.setFrom(finalStr);
-        mapping.setTo("/sources/java/myProject");
-        mapping.setIncludeds(new HashSet<String>());
-        mapping.setExcludeds(new HashSet<String>());
-        mapping.getIncludeds().add("*.java");
-        source.setMappings(new HashSet<ArtifactSourceMapping>());
-        source.getMappings().add(mapping);
-        source.setActive(true);
-        source.setInitialLookup(initial);
-        source.setName("sourceName");
+	@Test
+	public void shouldLoad() throws Exception {
+		final GlobalSettings configuration = new GlobalSettings();
+		configuration.setDefaultSleepingIntervalInMilliseconds(500);
+		configuration.setNumberOfParallelThreads(4);
+		final String initialRawPath = Files.getNormalizedFileName(new File(
+				"../.."));
+		final String initial = initialRawPath.substring(0, initialRawPath
+				.lastIndexOf('/'));
+		final String finalStr = initialRawPath.substring(initial.length());
+		final ArtifactSource source = new ArtifactSource();
+		final Repository repository = new Repository();
+		repository.setName("repository");
+		source.setRepository(repository);
+		final ArtifactSourceMapping mapping = new ArtifactSourceMapping();
+		mapping.setFrom(finalStr);
+		mapping.setTo("/sources/java/myProject");
+		mapping.setIncludeds(new HashSet<String>());
+		mapping.setExcludeds(new HashSet<String>());
+		mapping.getIncludeds().add("*.java");
+		source.setMappings(new HashSet<ArtifactSourceMapping>());
+		source.getMappings().add(mapping);
+		source.setActive(true);
+		source.setInitialLookup(initial);
+		source.setName("sourceName");
 
-        final ArtifactLoader loader = ArtifactLoaderFactory.createNewLoader(configuration,
-                                                                            new FileSystemArtifactBySourceProvider());
+		final ArtifactLoader loader = ArtifactLoaderFactory.createNewLoader(
+				configuration, new FileSystemArtifactBySourceProvider());
 
-        final Iterable<Artifact> artifacts = loader.loadArtifactsFromSource(source);
-        boolean hasAny = false;
-        
-        for (final Artifact a : artifacts) {
-            assertThat(a, is(notNullValue()));
-            assertThat(a.getArtifactCompleteName().startsWith(mapping.getTo()+"/"), is(true));
-            assertThat(a.getArtifactCompleteName().contains(mapping.getFrom()+"/"), is(false));
-            hasAny = true;
-        }
-        assertThat(hasAny, is(true));
-        loader.closeResources();
-    }
+		final Iterable<Artifact> artifacts = loader
+				.loadArtifactsFromSource(source);
+		boolean hasAny = false;
+
+		for (final Artifact a : artifacts) {
+			assertThat(a, is(notNullValue()));
+			assertThat(a.getArtifactCompleteName().startsWith(
+					mapping.getTo() + "/"), is(true));
+			assertThat(a.getArtifactCompleteName().contains(
+					mapping.getFrom() + "/"), is(false));
+			hasAny = true;
+		}
+		assertThat(hasAny, is(true));
+		loader.closeResources();
+	}
 
 }
