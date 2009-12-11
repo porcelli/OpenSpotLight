89ce6780c7b34e187853f8c6ccb36f0fabcbc6eb
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-12-11 10:35:38 -0200

    test done!

--- osl-federation/osl-federation-core/src/test/java/org/openspotlight/federation/util/test/GroupSupportTest.java
+++ osl-federation/osl-federation-core/src/test/java/org/openspotlight/federation/util/test/GroupSupportTest.java
@@ -90,36 +90,248 @@ public class GroupSupportTest {
 	@Test
 	public void shouldFindAddedGroupsWithinExistentRepositories()
 			throws Exception {
-		Assert.fail();
+
+		final Set<Repository> oldRepositories = new HashSet<Repository>();
+		final Repository oldRepository = new Repository();
+		oldRepositories.add(oldRepository);
+		oldRepository.setName("repositoryName");
+		final Group oldGroup = new Group();
+		oldGroup.setRepository(oldRepository);
+		oldGroup.setName("1");
+		oldRepository.getGroups().add(oldGroup);
+
+		final RepositoryGroupDifferences empty = new RepositoryGroupDifferences();
+		final Set<Repository> newRepositories = new HashSet<Repository>();
+		final Repository repository = new Repository();
+		newRepositories.add(repository);
+		repository.setName("repositoryName");
+		final Group newGroup = new Group();
+		newGroup.setRepository(repository);
+		newGroup.setName("1");
+		repository.getGroups().add(newGroup);
+		final Group newGroup2 = new Group();
+		newGroup2.setGroup(newGroup);
+		newGroup2.setName("2");
+		newGroup.getGroups().add(newGroup2);
+		GroupSupport.findDifferencesOnAllRepositories(empty, oldRepositories,
+				newRepositories);
+		Assert.assertThat(empty.getDifferencesByRepository().get(
+				"repositoryName"), Is.is(IsNull.notNullValue()));
+		Assert.assertThat(
+				empty.getDifferencesByRepository().get("repositoryName")
+						.getAddedGroups().contains("repositoryName/1"), Is
+						.is(false));
+		Assert.assertThat(empty.getDifferencesByRepository().get(
+				"repositoryName").getAddedGroups().contains(
+				"repositoryName/1/2"), Is.is(true));
 	}
 
 	@Test
 	public void shouldFindAddedGroupsWithinExistentRepositoriesOnDelta()
 			throws Exception {
-		Assert.fail();
+		final RepositoryGroupDifferences notEmpty = new RepositoryGroupDifferences();
+		final GroupDifferences differences = new GroupDifferences();
+		differences.setRepositoryName("repositoryName");
+		differences.setRepositoryGroupDifferences(notEmpty);
+		differences.getAddedGroups().add("repositoryName/existentBefore");
+		differences.getRemovedGroups().add("repositoryName/excludedBefore");
+		notEmpty.getDifferencesByRepository()
+				.put("repositoryName", differences);
+
+		final Set<Repository> oldRepositories = new HashSet<Repository>();
+		final Repository oldRepository = new Repository();
+		oldRepositories.add(oldRepository);
+		oldRepository.setName("repositoryName");
+		final Group oldGroup = new Group();
+		oldGroup.setRepository(oldRepository);
+		oldGroup.setName("1");
+		oldRepository.getGroups().add(oldGroup);
+
+		final Set<Repository> newRepositories = new HashSet<Repository>();
+		final Repository repository = new Repository();
+		newRepositories.add(repository);
+		repository.setName("repositoryName");
+		final Group newGroup = new Group();
+		newGroup.setRepository(repository);
+		newGroup.setName("1");
+		repository.getGroups().add(newGroup);
+		final Group newGroup2 = new Group();
+		newGroup2.setGroup(newGroup);
+		newGroup2.setName("2");
+		newGroup.getGroups().add(newGroup2);
+		GroupSupport.findDifferencesOnAllRepositories(notEmpty,
+				oldRepositories, newRepositories);
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName"), Is.is(IsNull.notNullValue()));
+		Assert.assertThat(
+				notEmpty.getDifferencesByRepository().get("repositoryName")
+						.getAddedGroups().contains("repositoryName/1"), Is
+						.is(false));
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName").getAddedGroups().contains(
+				"repositoryName/1/2"), Is.is(true));
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName").getAddedGroups().contains(
+				"repositoryName/existentBefore"), Is.is(true));
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName").getRemovedGroups().contains(
+				"repositoryName/excludedBefore"), Is.is(true));
 	}
 
 	@Test
-	public void shouldFindExcludedGroupsOnNewRepositories() throws Exception {
-		Assert.fail();
+	public void shouldFindExcludedGroupsOnOldRepositories() throws Exception {
+		final RepositoryGroupDifferences empty = new RepositoryGroupDifferences();
+		final Set<Repository> oldRepositories = new HashSet<Repository>();
+		final Repository repository = new Repository();
+		oldRepositories.add(repository);
+		repository.setName("repositoryName");
+		final Group newGroup = new Group();
+		newGroup.setRepository(repository);
+		newGroup.setName("1");
+		repository.getGroups().add(newGroup);
+		final Group newGroup2 = new Group();
+		newGroup2.setGroup(newGroup);
+		newGroup2.setName("2");
+		newGroup.getGroups().add(newGroup2);
+		GroupSupport.findDifferencesOnAllRepositories(empty, oldRepositories,
+				Collections.<Repository> emptySet());
+		Assert.assertThat(empty.getDifferencesByRepository().get(
+				"repositoryName"), Is.is(IsNull.notNullValue()));
+		Assert.assertThat(empty.getDifferencesByRepository().get(
+				"repositoryName").getRemovedGroups().contains(
+				"repositoryName/1"), Is.is(true));
+		Assert.assertThat(empty.getDifferencesByRepository().get(
+				"repositoryName").getRemovedGroups().contains(
+				"repositoryName/1/2"), Is.is(true));
 	}
 
 	@Test
-	public void shouldFindExcludedGroupsOnNewRepositoriesOnDelta()
+	public void shouldFindExcludedGroupsOnOldRepositoriesOnDelta()
 			throws Exception {
-		Assert.fail();
+		final RepositoryGroupDifferences notEmpty = new RepositoryGroupDifferences();
+		final GroupDifferences differences = new GroupDifferences();
+		differences.setRepositoryName("repositoryName");
+		differences.setRepositoryGroupDifferences(notEmpty);
+		differences.getAddedGroups().add("repositoryName/existentBefore");
+		differences.getRemovedGroups().add("repositoryName/excludedBefore");
+		notEmpty.getDifferencesByRepository()
+				.put("repositoryName", differences);
+		final Set<Repository> oldRepositories = new HashSet<Repository>();
+		final Repository repository = new Repository();
+		oldRepositories.add(repository);
+		repository.setName("repositoryName");
+		final Group newGroup = new Group();
+		newGroup.setRepository(repository);
+		newGroup.setName("1");
+		repository.getGroups().add(newGroup);
+		final Group newGroup2 = new Group();
+		newGroup2.setGroup(newGroup);
+		newGroup2.setName("2");
+		newGroup.getGroups().add(newGroup2);
+		GroupSupport.findDifferencesOnAllRepositories(notEmpty,
+				oldRepositories, Collections.<Repository> emptySet());
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName"), Is.is(IsNull.notNullValue()));
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName").getRemovedGroups().contains(
+				"repositoryName/1"), Is.is(true));
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName").getRemovedGroups().contains(
+				"repositoryName/1/2"), Is.is(true));
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName").getAddedGroups().contains(
+				"repositoryName/existentBefore"), Is.is(true));
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName").getRemovedGroups().contains(
+				"repositoryName/excludedBefore"), Is.is(true));
 	}
 
 	@Test
 	public void shouldFindExcludedGroupsWithinExistentRepositories()
 			throws Exception {
-		Assert.fail();
+		final Set<Repository> newRepositories = new HashSet<Repository>();
+		final Repository oldRepository = new Repository();
+		newRepositories.add(oldRepository);
+		oldRepository.setName("repositoryName");
+		final Group oldGroup = new Group();
+		oldGroup.setRepository(oldRepository);
+		oldGroup.setName("1");
+		oldRepository.getGroups().add(oldGroup);
+
+		final RepositoryGroupDifferences empty = new RepositoryGroupDifferences();
+		final Set<Repository> oldRepositories = new HashSet<Repository>();
+		final Repository repository = new Repository();
+		oldRepositories.add(repository);
+		repository.setName("repositoryName");
+		final Group newGroup = new Group();
+		newGroup.setRepository(repository);
+		newGroup.setName("1");
+		repository.getGroups().add(newGroup);
+		final Group newGroup2 = new Group();
+		newGroup2.setGroup(newGroup);
+		newGroup2.setName("2");
+		newGroup.getGroups().add(newGroup2);
+		GroupSupport.findDifferencesOnAllRepositories(empty, oldRepositories,
+				newRepositories);
+		Assert.assertThat(empty.getDifferencesByRepository().get(
+				"repositoryName"), Is.is(IsNull.notNullValue()));
+		Assert.assertThat(empty.getDifferencesByRepository().get(
+				"repositoryName").getRemovedGroups().contains(
+				"repositoryName/1"), Is.is(false));
+		Assert.assertThat(empty.getDifferencesByRepository().get(
+				"repositoryName").getRemovedGroups().contains(
+				"repositoryName/1/2"), Is.is(true));
 	}
 
 	@Test
 	public void shouldFindExcludedGroupsWithinExistentRepositoriesOnDelta()
 			throws Exception {
-		Assert.fail();
+		final RepositoryGroupDifferences notEmpty = new RepositoryGroupDifferences();
+		final GroupDifferences differences = new GroupDifferences();
+		differences.setRepositoryName("repositoryName");
+		differences.setRepositoryGroupDifferences(notEmpty);
+		differences.getAddedGroups().add("repositoryName/existentBefore");
+		differences.getRemovedGroups().add("repositoryName/excludedBefore");
+		notEmpty.getDifferencesByRepository()
+				.put("repositoryName", differences);
+
+		final Set<Repository> newRepositories = new HashSet<Repository>();
+		final Repository oldRepository = new Repository();
+		newRepositories.add(oldRepository);
+		oldRepository.setName("repositoryName");
+		final Group oldGroup = new Group();
+		oldGroup.setRepository(oldRepository);
+		oldGroup.setName("1");
+		oldRepository.getGroups().add(oldGroup);
+
+		final Set<Repository> oldRepositories = new HashSet<Repository>();
+		final Repository repository = new Repository();
+		oldRepositories.add(repository);
+		repository.setName("repositoryName");
+		final Group newGroup = new Group();
+		newGroup.setRepository(repository);
+		newGroup.setName("1");
+		repository.getGroups().add(newGroup);
+		final Group newGroup2 = new Group();
+		newGroup2.setGroup(newGroup);
+		newGroup2.setName("2");
+		newGroup.getGroups().add(newGroup2);
+		GroupSupport.findDifferencesOnAllRepositories(notEmpty,
+				oldRepositories, newRepositories);
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName"), Is.is(IsNull.notNullValue()));
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName").getRemovedGroups().contains(
+				"repositoryName/1"), Is.is(false));
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName").getRemovedGroups().contains(
+				"repositoryName/1/2"), Is.is(true));
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName").getAddedGroups().contains(
+				"repositoryName/existentBefore"), Is.is(true));
+		Assert.assertThat(notEmpty.getDifferencesByRepository().get(
+				"repositoryName").getRemovedGroups().contains(
+				"repositoryName/excludedBefore"), Is.is(true));
 	}
 
 }
