a3ac1412a69d7ad7ebfdc6cf954a977e927e1057
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2009-12-21 14:27:02 -0200

    FK & PK done. Not tested yet

--- bundle-processor/osl-db-bundle/src/main/java/org/openspotlight/bundle/db/processor/DbTableArtifactBundleProcessor.java
+++ bundle-processor/osl-db-bundle/src/main/java/org/openspotlight/bundle/db/processor/DbTableArtifactBundleProcessor.java
@@ -8,6 +8,7 @@ import org.openspotlight.bundle.db.metamodel.link.AbstractTypeBind;
 import org.openspotlight.bundle.db.metamodel.link.CatalogTableView;
 import org.openspotlight.bundle.db.metamodel.link.ColumnDataType;
 import org.openspotlight.bundle.db.metamodel.link.DatabaseSchema;
+import org.openspotlight.bundle.db.metamodel.link.ForeignKey;
 import org.openspotlight.bundle.db.metamodel.link.GroupDatabase;
 import org.openspotlight.bundle.db.metamodel.link.SchemaCatalog;
 import org.openspotlight.bundle.db.metamodel.link.SchemaTableView;
@@ -23,6 +24,7 @@ import org.openspotlight.bundle.db.metamodel.node.TableViewTable;
 import org.openspotlight.bundle.db.metamodel.node.TableViewView;
 import org.openspotlight.federation.context.ExecutionContext;
 import org.openspotlight.federation.domain.Artifact;
+import org.openspotlight.federation.domain.ExportedFk;
 import org.openspotlight.federation.domain.LastProcessStatus;
 import org.openspotlight.federation.domain.TableArtifact;
 import org.openspotlight.federation.domain.ViewArtifact;
@@ -118,6 +120,25 @@ public class DbTableArtifactBundleProcessor implements
 			context.getGraphSession().addLink(ColumnDataType.class, column,
 					dataType, false);
 			column.setDataType(dataType.getName());
+			column.setPk(c.getPkName());
+			for (final ExportedFk fk : c.getExportedFks()) {
+				final Schema thatSchema = database.addNode(Schema.class, fk
+						.getTableSchema());
+				final String thatCatalogName = fk.getTableCatalog();
+				Catalog thatCatalog = null;
+				if (thatCatalogName != null) {
+					thatCatalog = thatSchema.addNode(Catalog.class,
+							thatCatalogName);
+				}
+				final SLNode thatTableParent = thatCatalog != null ? thatCatalog
+						: thatSchema;
+				final TableView thatTable = thatTableParent.addNode(
+						TableView.class, fk.getTableName());
+				final Column thatColumn = thatTable.addNode(Column.class, fk
+						.getColumnName());
+				context.getGraphSession().addLink(ForeignKey.class, column,
+						thatColumn, false);
+			}
 			addedColumns.add(c.getName());
 		}
 		for (final SLNode n : new HashSet<SLNode>(table.getNodes())) {
