228f5e5feddac2d7ba582eae2a52baa89cbb132d
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2009-12-22 21:42:00 -0200

    missing copyright info!

--- bundle-processor/osl-db-bundle/src/main/java/org/openspotlight/bundle/db/metamodel/link/ServerDatabase.java
+++ bundle-processor/osl-db-bundle/src/main/java/org/openspotlight/bundle/db/metamodel/link/ServerDatabase.java
@@ -1,50 +1,50 @@
 
 /*
  * OpenSpotLight - Open Source IT Governance Platform
- *  
- * Copyright (c) 2009, CARAVELATECH CONSULTORIA E TECNOLOGIA EM INFORMATICA LTDA 
- * or third-party contributors as indicated by the @author tags or express 
- * copyright attribution statements applied by the authors.  All third-party 
- * contributions are distributed under license by CARAVELATECH CONSULTORIA E 
- * TECNOLOGIA EM INFORMATICA LTDA. 
- * 
- * This copyrighted material is made available to anyone wishing to use, modify, 
- * copy, or redistribute it subject to the terms and conditions of the GNU 
- * Lesser General Public License, as published by the Free Software Foundation. 
- * 
- * This program is distributed in the hope that it will be useful, 
+ *
+ * Copyright (c) 2009, CARAVELATECH CONSULTORIA E TECNOLOGIA EM INFORMATICA LTDA
+ * or third-party contributors as indicated by the @author tags or express
+ * copyright attribution statements applied by the authors.  All third-party
+ * contributions are distributed under license by CARAVELATECH CONSULTORIA E
+ * TECNOLOGIA EM INFORMATICA LTDA.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * See the GNU Lesser General Public License  for more details. 
- * 
- * You should have received a copy of the GNU Lesser General Public License 
- * along with this distribution; if not, write to: 
- * Free Software Foundation, Inc. 
- * 51 Franklin Street, Fifth Floor 
- * Boston, MA  02110-1301  USA 
- * 
- *********************************************************************** 
- * OpenSpotLight - Plataforma de Governan�a de TI de C�digo Aberto 
  *
- * Direitos Autorais Reservados (c) 2009, CARAVELATECH CONSULTORIA E TECNOLOGIA 
- * EM INFORMATICA LTDA ou como contribuidores terceiros indicados pela etiqueta 
- * @author ou por expressa atribui��o de direito autoral declarada e atribu�da pelo autor.
- * Todas as contribui��es de terceiros est�o distribu�das sob licen�a da
- * CARAVELATECH CONSULTORIA E TECNOLOGIA EM INFORMATICA LTDA. 
- * 
- * Este programa � software livre; voc� pode redistribu�-lo e/ou modific�-lo sob os 
- * termos da Licen�a P�blica Geral Menor do GNU conforme publicada pela Free Software 
- * Foundation. 
- * 
- * Este programa � distribu�do na expectativa de que seja �til, por�m, SEM NENHUMA 
- * GARANTIA; nem mesmo a garantia impl�cita de COMERCIABILIDADE OU ADEQUA��O A UMA
- * FINALIDADE ESPEC�FICA. Consulte a Licen�a P�blica Geral Menor do GNU para mais detalhes.  
- * 
- * Voc� deve ter recebido uma c�pia da Licen�a P�blica Geral Menor do GNU junto com este
- * programa; se n�o, escreva para: 
- * Free Software Foundation, Inc. 
- * 51 Franklin Street, Fifth Floor 
+ * See the GNU Lesser General Public License  for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ *
+ ***********************************************************************
+ * OpenSpotLight - Plataforma de Governança de TI de Código Aberto
+ *
+ * Direitos Autorais Reservados (c) 2009, CARAVELATECH CONSULTORIA E TECNOLOGIA
+ * EM INFORMATICA LTDA ou como contribuidores terceiros indicados pela etiqueta
+ * @author ou por expressa atribuição de direito autoral declarada e atribuída pelo autor.
+ * Todas as contribuições de terceiros estão distribuídas sob licença da
+ * CARAVELATECH CONSULTORIA E TECNOLOGIA EM INFORMATICA LTDA.
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo sob os
+ * termos da Licença Pública Geral Menor do GNU conforme publicada pela Free Software
+ * Foundation.
+ *
+ * Este programa é distribuído na expectativa de que seja útil, porém, SEM NENHUMA
+ * GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU ADEQUAÇÃO A UMA
+ * FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto com este
+ * programa; se não, escreva para:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
  * Boston, MA  02110-1301  USA
  */
 package org.openspotlight.bundle.db.metamodel.link;
