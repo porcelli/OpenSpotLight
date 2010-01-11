511cef511bd5edd686b3a639c9e71c15c6e98db0
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2010-01-11 20:18:24 -0200

    forgot to add some files :-)

---
+++ bundle-processor/osl-bundle-java/src/main/java/org/openspotlight/bundle/language/java/bundle/jar/JarProcessor.java
@@ -0,0 +1,36 @@
+package org.openspotlight.bundle.dap.language.java.bundle.jar;
+
+import org.openspotlight.federation.context.ExecutionContext;
+import org.openspotlight.federation.domain.artifact.LastProcessStatus;
+import org.openspotlight.federation.domain.artifact.StreamArtifact;
+import org.openspotlight.federation.processing.BundleProcessorArtifactPhase;
+import org.openspotlight.federation.processing.CurrentProcessorContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JarProcessor implements
+		BundleProcessorArtifactPhase<StreamArtifact> {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	public void beforeProcessArtifact(final StreamArtifact artifact) {
+
+	}
+
+	public void didFinishToProcessArtifact(final StreamArtifact artifact,
+			final LastProcessStatus status) {
+
+	}
+
+	public Class<StreamArtifact> getArtifactType() {
+		return StreamArtifact.class;
+	}
+
+	public LastProcessStatus processArtifact(final StreamArtifact artifact,
+			final CurrentProcessorContext currentContext,
+			final ExecutionContext context) throws Exception {
+		logger.info("processing " + artifact.getArtifactCompleteName());
+		return LastProcessStatus.PROCESSED;
+	}
+
+}
