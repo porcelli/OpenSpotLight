06c4208055c67b20ade4f2ff354a2e396e568c76
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2010-01-16 17:59:44 -0200

    before continue on federation refactor, just adding more tests

--- osl-common/src/test/java/org/openspotlight/common/task/test/TaskManagerTest.java
+++ osl-common/src/test/java/org/openspotlight/common/task/test/TaskManagerTest.java
@@ -2,6 +2,7 @@ package org.openspotlight.common.task.test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.hamcrest.core.Is;
@@ -18,61 +19,82 @@ public class TaskManagerTest {
 
 	private class Worker implements RunnableWithException {
 		private final String description;
-
 		private final List<String> list;
 
-		public Worker(final String description, final List<String> list) {
+		private final boolean sleeping;
+
+		public Worker(final String description, final List<String> list,
+				final boolean sleeping) {
 			this.description = description;
 			this.list = list;
+			this.sleeping = sleeping;
 		}
 
 		public void run() throws Exception {
+			if (sleeping) {
+				Thread.currentThread();
+				Thread.sleep(random.nextInt(500));
+			}
 			System.err.println("  >>> " + description);
 			list.add(description);
 		}
 
 	}
 
+	private static final Random random = new Random();
+
 	public static void main(final String... args) throws Exception {
 		new TaskManagerTest().shouldExecuteTasksOnPoolWithEigth();
 	}
 
+	private void createSleepingTasks(final TaskPool pool,
+			final List<String> list) {
+		createTasks(pool, list, true);
+	}
+
 	private void createTasks(final TaskPool pool, final List<String> list) {
+		createTasks(pool, list, false);
+	}
+
+	private void createTasks(final TaskPool pool, final List<String> list,
+			final boolean sleeping) {
 		final TaskGroup group2 = pool.createTaskGroup("group-2", 2);
 		final TaskGroup group1 = pool.createTaskGroup("group-1", 1);
 		final Task task5 = group2.prepareTask().withReadableDescription(
 				"thisThread").withUniqueId("5").withRunnable(
-				new Worker("5", list)).andPublishTask();
+				new Worker("5", list, sleeping)).andPublishTask();
 		final Task task6 = group2.prepareTask().withReadableDescription(
 				"thisThread").withUniqueId("6").withRunnable(
-				new Worker("6", list)).withParentTasks(task5).andPublishTask();
+				new Worker("6", list, sleeping)).withParentTasks(task5)
+				.andPublishTask();
 		group2.prepareTask().withReadableDescription("thisThread")
-				.withUniqueId("7").withRunnable(new Worker("7", list))
-				.withParentTasks(task6).andPublishTask();
+				.withUniqueId("7")
+				.withRunnable(new Worker("7", list, sleeping)).withParentTasks(
+						task6).andPublishTask();
 
 		final Task task1 = group1.prepareTask().withReadableDescription(
 				"thisThread").withUniqueId("1").withRunnable(
-				new Worker("1", list)).andPublishTask();
+				new Worker("1", list, sleeping)).andPublishTask();
 
 		final Task task1_1 = group1.prepareTask().withReadableDescription(
 				"thisThread-1_1").withUniqueId("1_1").withRunnable(
-				new Worker("1_1", list)).withParentTasks(task1)
+				new Worker("1_1", list, sleeping)).withParentTasks(task1)
 				.andPublishTask();
 
 		final Task task1_2 = group1.prepareTask().withReadableDescription(
 				"thisThread-1_2").withUniqueId("1_2").withRunnable(
-				new Worker("1_2", list)).withParentTasks(task1, task1_1)
-				.andPublishTask();
+				new Worker("1_2", list, sleeping)).withParentTasks(task1,
+				task1_1).andPublishTask();
 
 		final Task task1_3 = group1.prepareTask().withReadableDescription(
 				"thisThread-1_3").withUniqueId("1_3").withRunnable(
-				new Worker("1_3", list)).withParentTasks(task1, task1_2)
-				.andPublishTask();
+				new Worker("1_3", list, sleeping)).withParentTasks(task1,
+				task1_2).andPublishTask();
 
 		final Task task1_4 = group1.prepareTask().withReadableDescription(
 				"thisThread-1_4").withUniqueId("1_4").withRunnable(
-				new Worker("1_4", list)).withParentTasks(task1, task1_3)
-				.andPublishTask();
+				new Worker("1_4", list, sleeping)).withParentTasks(task1,
+				task1_3).andPublishTask();
 
 		group1.prepareTask().withReadableDescription("thisThread-taskFactory")
 				.withUniqueId("thisThread-taskFactory").withRunnable(
@@ -83,7 +105,8 @@ public class TaskManagerTest {
 										.withReadableDescription(
 												"thisThread-2_1").withUniqueId(
 												"2_1").withRunnable(
-												new Worker("2_1", list))
+												new Worker("2_1", list,
+														sleeping))
 										.withParentTasks(task1_1, task1_2,
 												task1_3, task1_4)
 										.andPublishTask();
@@ -92,7 +115,8 @@ public class TaskManagerTest {
 										.withReadableDescription(
 												"thisThread-2_2").withUniqueId(
 												"2_2").withRunnable(
-												new Worker("2_2", list))
+												new Worker("2_2", list,
+														sleeping))
 										.withParentTasks(task1_1, task1_2,
 												task1_3, task1_4, task2_1)
 										.andPublishTask();
@@ -101,7 +125,8 @@ public class TaskManagerTest {
 										.withReadableDescription(
 												"thisThread-2_3").withUniqueId(
 												"2_3").withRunnable(
-												new Worker("2_3", list))
+												new Worker("2_3", list,
+														sleeping))
 										.withParentTasks(task1_1, task1_2,
 												task1_3, task1_4, task2_2)
 										.andPublishTask();
@@ -110,7 +135,8 @@ public class TaskManagerTest {
 										.withReadableDescription(
 												"thisThread-2_4").withUniqueId(
 												"2_4").withRunnable(
-												new Worker("2_4", list))
+												new Worker("2_4", list,
+														sleeping))
 										.withParentTasks(task1_1, task1_2,
 												task1_3, task1_4, task2_3)
 										.andPublishTask();
@@ -118,21 +144,133 @@ public class TaskManagerTest {
 								final Task task3 = group1
 										.prepareTask()
 										.withReadableDescription("thisThread-3")
-										.withUniqueId("3").withRunnable(
-												new Worker("3", list))
+										.withUniqueId("3")
+										.withRunnable(
+												new Worker("3", list, sleeping))
 										.withParentTasks(task2_1, task2_2,
 												task2_3, task2_4)
 										.andPublishTask();
 
-								group1.prepareTask().withReadableDescription(
-										"thisThread-4").withUniqueId("4")
-										.withRunnable(new Worker("4", list))
+								group1
+										.prepareTask()
+										.withReadableDescription("thisThread-4")
+										.withUniqueId("4")
+										.withRunnable(
+												new Worker("4", list, sleeping))
 										.withParentTasks(task3)
 										.andPublishTask();
 
 							}
 						}).withParentTasks(task1, task1_4).andPublishTask();
 
+	}
+
+	@Test
+	public void shouldExecuteSleepingTasksOnPoolWithEigth() throws Exception {
+
+		final TaskPool pool = TaskManager.INSTANCE.createTaskPool("test-pool",
+				8);
+		final List<String> list = new CopyOnWriteArrayList<String>();
+		createSleepingTasks(pool, list);
+		pool.startExecutorBlockingUntilFinish();
+		Assert.assertThat(list, Is.is(Arrays.asList("1", "1_1", "1_2", "1_3",
+				"1_4", "2_1", "2_2", "2_3", "2_4", "3", "4", "5", "6", "7")));
+	}
+
+	@Test
+	public void shouldExecuteSleepingTasksOnPoolWithFive() throws Exception {
+
+		final TaskPool pool = TaskManager.INSTANCE.createTaskPool("test-pool",
+				5);
+		final List<String> list = new CopyOnWriteArrayList<String>();
+		createSleepingTasks(pool, list);
+		pool.startExecutorBlockingUntilFinish();
+		Assert.assertThat(list, Is.is(Arrays.asList("1", "1_1", "1_2", "1_3",
+				"1_4", "2_1", "2_2", "2_3", "2_4", "3", "4", "5", "6", "7")));
+	}
+
+	@Test
+	public void shouldExecuteSleepingTasksOnPoolWithFour() throws Exception {
+
+		final TaskPool pool = TaskManager.INSTANCE.createTaskPool("test-pool",
+				4);
+		final List<String> list = new CopyOnWriteArrayList<String>();
+		createSleepingTasks(pool, list);
+		pool.startExecutorBlockingUntilFinish();
+		Assert.assertThat(list, Is.is(Arrays.asList("1", "1_1", "1_2", "1_3",
+				"1_4", "2_1", "2_2", "2_3", "2_4", "3", "4", "5", "6", "7")));
+	}
+
+	@Test
+	public void shouldExecuteSleepingTasksOnPoolWithSeven() throws Exception {
+
+		final TaskPool pool = TaskManager.INSTANCE.createTaskPool("test-pool",
+				7);
+		final List<String> list = new CopyOnWriteArrayList<String>();
+		createSleepingTasks(pool, list);
+		pool.startExecutorBlockingUntilFinish();
+		Assert.assertThat(list, Is.is(Arrays.asList("1", "1_1", "1_2", "1_3",
+				"1_4", "2_1", "2_2", "2_3", "2_4", "3", "4", "5", "6", "7")));
+	}
+
+	@Test
+	public void shouldExecuteSleepingTasksOnPoolWithSix() throws Exception {
+
+		final TaskPool pool = TaskManager.INSTANCE.createTaskPool("test-pool",
+				6);
+		final List<String> list = new CopyOnWriteArrayList<String>();
+		createSleepingTasks(pool, list);
+		pool.startExecutorBlockingUntilFinish();
+		Assert.assertThat(list, Is.is(Arrays.asList("1", "1_1", "1_2", "1_3",
+				"1_4", "2_1", "2_2", "2_3", "2_4", "3", "4", "5", "6", "7")));
+	}
+
+	@Test
+	public void shouldExecuteSleepingTasksOnPoolWithSixteen() throws Exception {
+
+		final TaskPool pool = TaskManager.INSTANCE.createTaskPool("test-pool",
+				16);
+		final List<String> list = new CopyOnWriteArrayList<String>();
+		createSleepingTasks(pool, list);
+		pool.startExecutorBlockingUntilFinish();
+		Assert.assertThat(list, Is.is(Arrays.asList("1", "1_1", "1_2", "1_3",
+				"1_4", "2_1", "2_2", "2_3", "2_4", "3", "4", "5", "6", "7")));
+	}
+
+	@Test
+	public void shouldExecuteSleepingTasksOnPoolWithThree() throws Exception {
+
+		final TaskPool pool = TaskManager.INSTANCE.createTaskPool("test-pool",
+				3);
+		final List<String> list = new CopyOnWriteArrayList<String>();
+		createSleepingTasks(pool, list);
+		pool.startExecutorBlockingUntilFinish();
+		Assert.assertThat(list, Is.is(Arrays.asList("1", "1_1", "1_2", "1_3",
+				"1_4", "2_1", "2_2", "2_3", "2_4", "3", "4", "5", "6", "7")));
+	}
+
+	@Test
+	public void shouldExecuteSleepingTasksOnPoolWithTwoTasks() throws Exception {
+
+		final TaskPool pool = TaskManager.INSTANCE.createTaskPool("test-pool",
+				2);
+		final List<String> list = new CopyOnWriteArrayList<String>();
+		createSleepingTasks(pool, list);
+		pool.startExecutorBlockingUntilFinish();
+		Assert.assertThat(list, Is.is(Arrays.asList("1", "1_1", "1_2", "1_3",
+				"1_4", "2_1", "2_2", "2_3", "2_4", "3", "4", "5", "6", "7")));
+	}
+
+	@Test
+	public void shouldExecuteSleepingTasksOnSinglePool() throws Exception {
+
+		final TaskPool pool = TaskManager.INSTANCE.createTaskPool("test-pool",
+				1);
+		final List<String> list = new CopyOnWriteArrayList<String>();
+		createSleepingTasks(pool, list);
+		pool.startExecutorBlockingUntilFinish();
+		Assert.assertThat(list, Is.is(Arrays.asList("1", "1_1", "1_2", "1_3",
+				"1_4", "2_1", "2_2", "2_3", "2_4", "3", "4", "5", "6", "7")));
 	}
 
 	@Test
