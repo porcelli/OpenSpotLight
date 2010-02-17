15d1b38277b95a3d74838500c171632315ec80f3
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2010-02-17 14:55:59 -0200

    preparing tests

---
+++ bundle-processor/osl-bundle-java/src/test/java/org/openspotlight/bundle/language/java/bundle/test/JavaBundleTest.java
@@ -0,0 +1,118 @@
+package org.openspotlight.bundle.language.java.bundle.test;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.openspotlight.bundle.common.AbstractTestServerClass;
+import org.openspotlight.bundle.language.java.JavaConstants;
+import org.openspotlight.bundle.language.java.bundle.JavaBinaryProcessor;
+import org.openspotlight.bundle.language.java.bundle.JavaGlobalPhase;
+import org.openspotlight.bundle.language.java.bundle.JavaLexerAndParserTypesPhase;
+import org.openspotlight.bundle.language.java.bundle.JavaParserPublicElementsPhase;
+import org.openspotlight.federation.context.ExecutionContext;
+import org.openspotlight.federation.context.ExecutionContextFactory;
+import org.openspotlight.federation.context.TestExecutionContextFactory;
+import org.openspotlight.federation.context.TestExecutionContextFactory.ArtifactFinderType;
+import org.openspotlight.federation.domain.BundleProcessorType;
+import org.openspotlight.federation.domain.BundleSource;
+import org.openspotlight.federation.domain.GlobalSettings;
+import org.openspotlight.federation.domain.Group;
+import org.openspotlight.federation.domain.Repository;
+import org.openspotlight.federation.domain.artifact.ArtifactSource;
+import org.openspotlight.federation.processing.DefaultBundleProcessorManager;
+import org.openspotlight.federation.processing.BundleProcessorManager.GlobalExecutionStatus;
+import org.openspotlight.federation.scheduler.GlobalSettingsSupport;
+import org.openspotlight.jcr.provider.DefaultJcrDescriptor;
+import org.openspotlight.jcr.provider.JcrConnectionDescriptor;
+import org.openspotlight.jcr.provider.JcrConnectionProvider;
+
+public class JavaBundleTest extends AbstractTestServerClass {
+
+	public static void main(final String... args) {
+		final JavaBundleTest test = new JavaBundleTest();
+		test.doWorkAndExposeServers();
+	}
+
+	private String repositoryName;
+	private ExecutionContextFactory contextFactory;
+	private GlobalSettings settings;
+	private Group group;
+	private final String username = "sa";
+	private final String password = "sa";
+
+	@Override
+	protected void doWork(final JcrConnectionProvider provider)
+	throws Exception {
+		final Repository repo = new Repository();
+		repo.setName("name");
+		repo.setActive(true);
+		repositoryName = repo.getName();
+		final ArtifactSource includedSource = new ArtifactSource();
+		includedSource.setRepository(repo);
+		includedSource.setName("classpath");
+		includedSource
+		.setInitialLookup("./src/test/resources/junit-4.3.1");
+		contextFactory = TestExecutionContextFactory.createFactory(
+				ArtifactFinderType.FILESYSTEM, includedSource);
+
+		settings = new GlobalSettings();
+		settings.setDefaultSleepingIntervalInMilliseconds(1000);
+		settings.setNumberOfParallelThreads(1);
+		settings
+		.setArtifactFinderRegistryClass(SampleJavaArtifactRegistry.class);
+		GlobalSettingsSupport.initializeScheduleMap(settings);
+		group = new Group();
+		group.setName("sampleGroup");
+		group.setRepository(repo);
+		repo.getGroups().add(group);
+		group.setActive(true);
+
+		final BundleProcessorType jarProcessor = new BundleProcessorType();
+		jarProcessor.setActive(true);
+		jarProcessor.setGroup(group);
+		jarProcessor.setGlobalPhase(JavaGlobalPhase.class);
+		jarProcessor.getArtifactPhases().add(JavaBinaryProcessor.class);
+		group.getBundleTypes().add(jarProcessor);
+
+		final BundleSource jarSource = new BundleSource();
+		jarProcessor.getSources().add(jarSource);
+		jarSource.setBundleProcessorType(jarProcessor);
+		jarSource.setRelative("/jar");
+		jarSource.getIncludeds().add("/jar/luni-few-classes.jar");
+
+		final BundleProcessorType commonProcessor = new BundleProcessorType();
+		commonProcessor.getBundleProperties().put(JavaConstants.JAR_CLASSPATH,
+		"jar/luni-few-classes.jar");
+		commonProcessor.setActive(true);
+		commonProcessor.setName("source processor");
+		commonProcessor.setGroup(group);
+		commonProcessor.setGlobalPhase(JavaGlobalPhase.class);
+		commonProcessor.getArtifactPhases().add(
+				JavaLexerAndParserTypesPhase.class);
+		commonProcessor.getArtifactPhases().add(
+				JavaParserPublicElementsPhase.class);
+		group.getBundleTypes().add(commonProcessor);
+
+		final BundleSource bundleSource = new BundleSource();
+		commonProcessor.getSources().add(bundleSource);
+		bundleSource.setBundleProcessorType(commonProcessor);
+		bundleSource.setRelative("src/");
+		bundleSource.getIncludeds().add("**/*.java");
+		final ExecutionContext ctx = contextFactory
+		.createExecutionContext(username, password, getDescriptor(),
+				repositoryName);
+		ctx.getDefaultConfigurationManager().saveGlobalSettings(settings);
+		ctx.getDefaultConfigurationManager().saveRepository(repo);
+
+		final GlobalExecutionStatus result = DefaultBundleProcessorManager.INSTANCE
+		.executeBundles(username, password, getDescriptor(),
+				contextFactory, settings, group);
+		Assert.assertThat(result, Is.is(GlobalExecutionStatus.SUCCESS));
+
+	}
+
+	@Override
+	protected JcrConnectionDescriptor getDescriptor() {
+		return DefaultJcrDescriptor.TEMP_DESCRIPTOR;
+	}
+
+}
---
+++ bundle-processor/osl-bundle-java/src/test/java/org/openspotlight/bundle/language/java/bundle/test/MainClass.java
@@ -0,0 +1,59 @@
+package org.openspotlight.bundle.language.java.bundle.test;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import org.apache.jackrabbit.rmi.remote.RemoteRepository;
+import org.apache.jackrabbit.rmi.server.RemoteAdapterFactory;
+import org.apache.jackrabbit.rmi.server.ServerAdapterFactory;
+import org.openspotlight.graph.server.RemoteGraphSessionServer;
+import org.openspotlight.jcr.provider.DefaultJcrDescriptor;
+import org.openspotlight.jcr.provider.JcrConnectionProvider;
+import org.openspotlight.remote.server.UserAuthenticator;
+
+public class MainClass {
+
+	public static void main(final String... args) throws Exception {
+		final long start = System.currentTimeMillis();
+		JcrConnectionProvider.createFromData(
+				DefaultJcrDescriptor.DEFAULT_DESCRIPTOR).openSession();
+		final javax.jcr.Repository repository = JcrConnectionProvider
+				.createFromData(DefaultJcrDescriptor.DEFAULT_DESCRIPTOR)
+				.getRepository();
+
+		final RemoteAdapterFactory saFactory = new ServerAdapterFactory();
+		final RemoteRepository remote = saFactory
+				.getRemoteRepository(repository);
+
+		final Registry registry = LocateRegistry
+				.createRegistry(Registry.REGISTRY_PORT);
+		registry.bind("jackrabbit.repository", remote);
+		final long end = System.currentTimeMillis();
+		System.err.println("started in about " + (int) (end - start) / 1000);
+
+		RemoteGraphSessionServer server = null;
+		try {
+			server = new RemoteGraphSessionServer(new UserAuthenticator() {
+
+				public boolean canConnect(final String userName,
+						final String password, final String clientHost) {
+					return true;
+				}
+
+				public boolean equals(final Object o) {
+					return this.getClass().equals(o.getClass());
+				}
+			}, 7070, 60 * 1000 * 10L, DefaultJcrDescriptor.DEFAULT_DESCRIPTOR);
+			System.err.println("Server waiting connections on port 7070");
+			while (true) {
+				Thread.sleep(5000);
+			}
+		} finally {
+			if (server != null) {
+				server.shutdown();
+			}
+		}
+
+	}
+
+}
