20bf9247fa2e173381d2d33c8f7c986b626fb9fc
Luiz Fernando Teston - Aka Feu Teston <feu.teston@gmail.com>
2010-03-13 17:53:27 -0300

    forgot few files ;-)

---
+++ osl-federation/osl-federation-core/src/main/java/org/openspotlight/federation/finder/AbstractOriginArtifactLoader.java
@@ -0,0 +1,154 @@
+/*
+ * OpenSpotLight - Open Source IT Governance Platform
+ *
+ * Copyright (c) 2009, CARAVELATECH CONSULTORIA E TECNOLOGIA EM INFORMATICA LTDA
+ * or third-party contributors as indicated by the @author tags or express
+ * copyright attribution statements applied by the authors.  All third-party
+ * contributions are distributed under license by CARAVELATECH CONSULTORIA E
+ * TECNOLOGIA EM INFORMATICA LTDA.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License  for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ *
+ ***********************************************************************
+ * OpenSpotLight - Plataforma de Governança de TI de Código Aberto
+ *
+ * Direitos Autorais Reservados (c) 2009, CARAVELATECH CONSULTORIA E TECNOLOGIA
+ * EM INFORMATICA LTDA ou como contribuidores terceiros indicados pela etiqueta
+ * @author ou por expressa atribuição de direito autoral declarada e atribuída pelo autor.
+ * Todas as contribuições de terceiros estão distribuídas sob licença da
+ * CARAVELATECH CONSULTORIA E TECNOLOGIA EM INFORMATICA LTDA.
+ *
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo sob os
+ * termos da Licença Pública Geral Menor do GNU conforme publicada pela Free Software
+ * Foundation.
+ *
+ * Este programa é distribuído na expectativa de que seja útil, porém, SEM NENHUMA
+ * GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU ADEQUAÇÃO A UMA
+ * FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor do GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto com este
+ * programa; se não, escreva para:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.openspotlight.federation.finder;
+
+import java.util.Set;
+
+import org.openspotlight.federation.domain.artifact.Artifact;
+import org.openspotlight.federation.domain.artifact.ArtifactSource;
+import org.openspotlight.federation.domain.artifact.PathElement;
+
+public abstract class AbstractOriginArtifactLoader implements
+		OriginArtifactLoader {
+
+	private final LoaderInternalMethods internalMethods = new LoaderInternalMethodsImpl();
+
+	private final class LoaderInternalMethodsImpl implements
+			LoaderInternalMethods {
+
+		public final Set<Class<? extends Artifact>> getAvailableTypes() {
+			return internalGetAvailableTypes();
+		}
+
+		public final <A extends Artifact> boolean isMaybeChanged(
+				String artifactName, A oldOne) {
+			return internalIsMaybeChanged(artifactName, oldOne);
+		}
+
+		public final boolean isTypeSupported(Class<? extends Artifact> type) {
+			return internalIsTypeSupported(type);
+		}
+
+		public final <A extends Artifact> Set<String> retrieveOriginalNames(
+				Class<A> type, ArtifactSource source, String initialPath) {
+			return internalRetrieveOriginalNames(type, source, initialPath);
+		}
+
+	}
+
+	public final <A extends Artifact> A findByPath(Class<A> type,
+			ArtifactSource source, String path) {
+		return fillSomeData(type, source,
+				internalFindByPath(type, source, path));
+	}
+
+	public final <A extends Artifact> A findByRelativePath(Class<A> type,
+			ArtifactSource source, A relativeTo, String path) {
+		return fillSomeData(type, source, internalFindByRelativePath(type,
+				source, relativeTo, path));
+	}
+
+	public final LoaderInternalMethods getInternalMethods() {
+		return internalMethods;
+	}
+
+	public final <A extends Artifact> Set<A> listByPath(Class<A> type,
+			ArtifactSource source, String path) {
+		return fillSomeData(type, source,
+				internalListByPath(type, source, path));
+	}
+
+	public final void closeResources() {
+		internalCloseResources();
+
+	}
+
+	protected abstract <A extends Artifact> boolean internalIsMaybeChanged(
+			String artifactName, A oldOne);
+
+	protected abstract Set<Class<? extends Artifact>> internalGetAvailableTypes();
+
+	protected abstract boolean internalIsTypeSupported(
+			Class<? extends Artifact> type);
+
+	protected abstract <A extends Artifact> Set<String> internalRetrieveOriginalNames(
+			Class<A> type, ArtifactSource source, String initialPath);
+
+	protected abstract <A extends Artifact> A internalFindByPath(Class<A> type,
+			ArtifactSource source, String path);
+
+	protected <A extends Artifact> A internalFindByRelativePath(Class<A> type,
+			ArtifactSource source, A relativeTo, String path) {
+		final String newPath = PathElement.createRelativePath(
+				relativeTo.getParent(), path).getCompletePath();
+		return internalFindByPath(type, source, newPath);
+
+	}
+
+	protected <A extends Artifact> Set<A> internalListByPath(
+			Class<A> type, ArtifactSource source, String path){
+		
+	}
+
+	private <A extends Artifact> A fillSomeData(Class<A> type,
+			ArtifactSource source, A artifact) {
+		artifact.setRepositoryName(source.getRepository().getName());
+		return artifact;
+	}
+
+	private <A extends Artifact> Set<A> fillSomeData(Class<A> type,
+			ArtifactSource source, Set<A> artifacts) {
+		for (A artifact : artifacts)
+			artifact.setRepositoryName(source.getRepository().getName());
+		return artifacts;
+	}
+
+	protected abstract void internalCloseResources();
+
+}
